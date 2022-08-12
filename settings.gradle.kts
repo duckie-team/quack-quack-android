7e7cae9701fe5f9611406c66bf74de718d3b8c03
jisungbin <ji@sungb.in>
2022-08-12 22:24:17 +0900

    cleanup includes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
-include(":common")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "quack-quack"
 include(
+    ":common",
     ":playground",
     ":lint-compose",
     ":lint-core",
