a1f287b8cf47fc268676bffbb8ee10d12c545a94
jisungbin <ji@sungb.in>
2022-08-13 12:12:06 +0900

    Fix typo linkChecksOverride to lintChecksOverride

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -29,6 +29,6 @@ android {
 
 dependencies {
     implementations(projects.uiComponents)
-    linkChecksOverride(projects.lintCore, projects.lintCompose, projects.lintWriting)
+    lintChecksOverride(projects.lintCore, projects.lintCompose, projects.lintWriting)
     setupCompose(core = libs.bundles.compose.core, debug = libs.bundles.compose.debug)
 }
