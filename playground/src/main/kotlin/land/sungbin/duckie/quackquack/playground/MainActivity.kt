0ff8f9f8b18944c9bb6e39c9355bf204d68225db
jisungbin <ji@sungb.in>
2022-08-13 20:13:01 +0900

    Update playground activity

--- playground/src/main/kotlin/land/sungbin/duckie/quackquack/playground/MainActivity.kt
+++ playground/src/main/kotlin/land/sungbin/duckie/quackquack/playground/MainActivity.kt
@@ -11,13 +11,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicText(text = stringResource(R.string.app_name))
+            BasicText(text = "AWESOME GRADLE!")
         }
     }
 }
