fb0198455657018ba644cf2bae5dc5ef169de1ac
jisungbin <ji@sungb.in>
2022-08-16 19:53:00 +0900

    refactor: 사용하지 않은 DependencyScope 확장 함수 제거

--- buildSrc/src/main/kotlin/GradleScopeExtensions.kt
+++ buildSrc/src/main/kotlin/GradleScopeExtensions.kt
@@ -11,46 +11,34 @@ import org.gradle.api.artifacts.dsl.DependencyHandler as DependencyScope
 import org.gradle.api.artifacts.ProjectDependency
 
 private const val Implementation = "implementation"
-private const val DebugImplementation = "debugImplementation"
-private const val TestRuntimeOnly = "testRuntimeOnly"
-private const val TestImplementation = "testImplementation"
-private const val CompileOnly = "compileOnly"
 private const val LintPublish = "lintPublish"
 private const val Api = "api"
 
-fun DependencyScope.setupJunit(core: Any, engine: Any) {
-    delegate(method = TestImplementation, core)
-    delegate(method = TestRuntimeOnly, engine)
-}
-
-fun DependencyScope.setupLint(core: Any, test: Any) {
-    delegate(method = CompileOnly, core)
-    delegate(method = TestImplementation, test)
-}
-
-fun DependencyScope.setupCompose(core: Any, debug: Any) {
-    delegate(method = Implementation, core)
-    delegate(method = DebugImplementation, debug)
-}
-
 fun DependencyScope.lintPublishs(vararg paths: Any) {
-    delegate(method = LintPublish, paths = paths)
+    delegate(
+        method = LintPublish,
+        paths = paths,
+    )
 }
 
 fun DependencyScope.implementations(vararg paths: Any) {
-    delegate(method = Implementation, paths = paths)
-}
-
-fun DependencyScope.debugImplementations(vararg paths: Any) {
-    delegate(method = DebugImplementation, paths = paths)
+    delegate(
+        method = Implementation,
+        paths = paths,
+    )
 }
 
 fun DependencyScope.apis(vararg paths: Any) {
-    delegate(method = Api, paths = paths)
+    delegate(
+        method = Api,
+        paths = paths,
+    )
 }
 
 private fun DependencyScope.delegate(method: String, vararg paths: Any) {
-    paths.forEach { path -> add(method, path) }
+    paths.forEach { path ->
+        add(method, path)
+    }
 }
 
 private fun DependencyScope.project(path: String) =
