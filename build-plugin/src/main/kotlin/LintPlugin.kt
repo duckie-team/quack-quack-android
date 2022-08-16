bc11bab4f962f75f17886f303886f6da4e06a229
jisungbin <ji@sungb.in>
2022-08-16 23:46:26 +0900

    test: compose 린트 테스트 코드 추가

--- build-plugin/src/main/kotlin/LintPlugin.kt
+++ build-plugin/src/main/kotlin/LintPlugin.kt
@@ -1,6 +1,14 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [LintPlugin.kt] created by Ji Sungbin on 22. 8. 16. 오후 7:29
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
 import land.sungbin.duckie.quackquack.plugin.PluginEnum
 import land.sungbin.duckie.quackquack.plugin.applyPlugins
-import land.sungbin.duckie.quackquack.plugin.compileOnlys
 import land.sungbin.duckie.quackquack.plugin.libs
 import land.sungbin.duckie.quackquack.plugin.setupJunit
 import land.sungbin.duckie.quackquack.plugin.setupLint
@@ -10,15 +18,6 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-
-/*
- * Designed and developed by 2022 SungbinLand, Team Duckie
- *
- * [LintPlugin.kt] created by Ji Sungbin on 22. 8. 16. 오후 7:29
- *
- * Licensed under the MIT.
- * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
- */
 
 internal class LintPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -37,16 +36,13 @@ internal class LintPlugin : Plugin<Project> {
 
             dependencies {
                 dependencies {
-                    compileOnlys(
-                        libs.findLibrary("kotlin-stdlib").get(),
-                    )
                     setupJunit(
                         core = libs.findLibrary("test-junit-core").get(),
                         engine = libs.findLibrary("test-junit-engine").get(),
                     )
                     setupLint(
-                        core = libs.findBundle("lint").get(),
-                        test = libs.findLibrary("test-lint").get(),
+                        core = libs.findLibrary("lint-api").get(),
+                        test = libs.findBundle("test-lint").get()
                     )
                 }
             }
