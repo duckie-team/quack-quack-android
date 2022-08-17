19500dc8edb0cc26bddfcd2a177cd2cf0785f2ef
jisungbin <ji@sungb.in>
2022-08-18 04:43:29 +0900

    chore: 앱 빌드 버전 업데이트

--- gradle-conventions/src/main/kotlin/team/duckie/quackquack/convention/ApplicationConstants.kt
+++ gradle-conventions/src/main/kotlin/team/duckie/quackquack/convention/ApplicationConstants.kt
@@ -16,7 +16,7 @@ internal object ApplicationConstants {
     const val targetSdk = 33
     const val compileSdk = 33
     const val jvmTarget = "11"
-    const val versionCode = 11
-    const val versionName = "0.1.0"
+    const val versionCode = 2
+    const val versionName = "0.1.1"
     val javaVersion = JavaVersion.VERSION_11
 }
