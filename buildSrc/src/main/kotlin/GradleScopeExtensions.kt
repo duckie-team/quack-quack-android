88820f7355d20996fee23a4f46ff846258c1d308
jisungbin <ji@sungb.in>
2022-08-19 23:26:10 +0900

    chore: customLints 및 bundleInsides 그레이들 확장 함수 추가

--- buildSrc/src/main/kotlin/GradleScopeExtensions.kt
+++ buildSrc/src/main/kotlin/GradleScopeExtensions.kt
@@ -9,9 +9,25 @@
 
 import org.gradle.api.artifacts.dsl.DependencyHandler as DependencyScope
 
+private const val BundleInside = "bundleInside"
 private const val Implementation = "implementation"
 private const val TestImplementation = "testImplementation"
 private const val Api = "api"
+private const val LintChecks = "lintChecks"
+
+fun DependencyScope.customLints(vararg paths: Any) {
+    delegate(
+        method = LintChecks,
+        paths = paths
+    )
+}
+
+fun DependencyScope.bundleInsides(vararg paths: Any) {
+    delegate(
+        method = BundleInside,
+        paths = paths
+    )
+}
 
 fun DependencyScope.implementations(vararg paths: Any) {
     delegate(
