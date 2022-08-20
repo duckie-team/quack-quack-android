2079a2f4a9d8ab13bc3d591463fcd963b61f9da2
jisungbin <ji@sungb.in>
2022-08-20 17:30:57 +0900

    refactor: 코드 정리

---
+++ common-lint/src/main/kotlin/team/duckie/quackquack/common/lint/empty.kt
@@ -0,0 +1,12 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [empty.kt] created by Ji Sungbin on 22. 8. 20. 오후 4:16
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.common.lint
+
+class Empty
