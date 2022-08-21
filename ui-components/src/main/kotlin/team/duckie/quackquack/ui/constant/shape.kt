88b8d1e42657d319f05eed34123732c9e8efe84d
goddoro <dongsang1222@daum.net>
2022-08-21 15:08:05 +0900

    feat: QuackTagShape 추가

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/shape.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/shape.kt
@@ -1,2 +1,15 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [shape.kt] created by Ji Sungbin on 22. 8. 21. 오후 2:54
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
 package team.duckie.quackquack.ui.constant
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+internal val QuackTagShape = RoundedCornerShape(18.dp)
