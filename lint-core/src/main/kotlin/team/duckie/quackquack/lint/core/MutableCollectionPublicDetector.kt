01d7301ef4c5c8a020582611551a69a009fd2ea3
ricky_0_k <ridickle7@gmail.com>
2022-08-23 08:32:00 +0900

    MutableCollectionPublic 케이스 작성 (#48)
    * feat: core lint registry 설정
    
    * feat: Composable 을 사용하지 않는 TestFile 케이스 추가 (Default) 및 enum 으로 관리
    
    * feat: MutableCollectionPublicIssue 생성
    
    * feat: MutableCollectionPublicIssue 에 대한 테스트 케이스 생성
    
    * feat: 성빈님 리뷰대로 getApplicableUastType 범위 확장 및 이에 따른 코드 수정
    
    * bug: 잘못 작성된 주석 수정 및 ktlintFormat
    
    * feat: 린트 팀과 논의했던 대로 Composable 가 없는 경우를 따로 두지 않음
    
    * feat: 코드리뷰 내용 반영
    
    1. 빠진 ',' 추가
    2. 이상한 문법 주석 수정
    3. Mutable Collection, MutableCollections -> MutableCollection
    4. 애매한 주석내용 보강
    5. 함수 내 파라미터 체크 케이스 제거
    6. 코틀린 문법에 맞춰, TestFile 내 변수 기본값 추가
    7. Don't use Public at Mutable Collection in class, object, interface 을 Don't use Public at Mutable Collection 에 합침
    
    * feat: empty 파일 제거
    
    * bug: 디버깅 원활화를 위해 변수 이름 변경 & 다른 체크 로직을 사용하면서 internal 도 에러로 체크하던 내용 수정
    
    * feat: 코드리뷰대로 주석 명확화
    
    * bug: Kotlin 특성 상 field 이외의 것 (ex. getter 등) 까지 lint 를 체크하던 이슈 해결
    
    * bug: Java interface 와 Kotlin interface 차이로 인해 lint 체크를 하지 않는 문제 수정 (interface 는 체크하지 않음)
    
    * feat: 코드리뷰대로 Collection 에 s 붙임
    
    * feat: 코드리뷰대로 함수명 변경 (영작)
    
    * docs: 클래스명 오타 수정
    
    ImMutableCollections 에서 ImmutableCollections 으로 변경
    
    Co-authored-by: Ji Sungbin <ji@sungb.in>

---
+++ lint-core/src/main/kotlin/team/duckie/quackquack/lint/core/MutableCollectionPublicDetector.kt
@@ -0,0 +1,105 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [MutableCollectionPublicDetector.kt] created by ricky_0_k on 22. 8. 21. 오전 12:43
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+@file:Suppress(
+    "UnstableApiUsage",
+    "SameParameterValue",
+)
+
+package team.duckie.quackquack.lint.core
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
+import org.jetbrains.uast.UDeclaration
+import org.jetbrains.uast.kotlin.KotlinUField
+
+private const val BriefDescription = "MutableCollections public 노출 금지"
+private const val Explanation = "캡슐화를 위해 MutableCollections public 노출은 금지됩니다."
+
+val MutableCollectionPublicIssue = Issue.create(
+    id = "MutableCollectionPublic",
+    briefDescription = BriefDescription,
+    explanation = Explanation,
+    category = Category.PERFORMANCE,
+    priority = 7,
+    severity = Severity.ERROR,
+    implementation = Implementation(
+        MutableCollectionPublicDetector::class.java,
+        Scope.JAVA_FILE_SCOPE
+    )
+)
+
+/**
+ * MutableCollections 접두사
+ *
+ * kotlinx.collections 패키지에 있는 Collections 들 입니다.
+ *
+ * @see [https://github.com/Kotlin/kotlinx.collections.immutable]
+ */
+private val MutableNames = listOf(
+    "Mutable",
+    "PersistentCollection.Builder",
+)
+
+/**
+ * QuackQuack 린트의 MutableCollectionPublic 규칙을 구현합니다.
+ *
+ * 다음과 같은 조건에서 린트 검사를 진행합니다.
+ *
+ * 1. MutableCollections 에서 public 을 사용할 시
+ *
+ * 현재 이 규칙은 MutableCollections 변수의 접근 제어 범위(visibility) 가 public 인지 검사하도록 구현됐습니다.
+ * 따라서 아래와 같이 MutableCollections 접근 제어 범위가 public 일 시
+ * 린트 에러가 발생합니다.
+ *
+ * ```
+ * val variableName: MutableCollection<Any> = emptyList()
+//     ~~~~~~~~~~~~ <- MutableCollections 의 접근 제어가 public 으로 감지됨
+ * ```
+ * TODO internal val variableName: MutableCollection<Any> <- internal 은 private 으로 감지됨
+ *      internal 에 대해서는 모니터링 필요
+ */
+class MutableCollectionPublicDetector : Detector(), SourceCodeScanner {
+    override fun getApplicableUastTypes() = listOf(
+        UDeclaration::class.java,
+    )
+
+    override fun createUastHandler(context: JavaContext) = object : UElementHandler() {
+        override fun visitDeclaration(node: UDeclaration) {
+            // Kotlin 특성 상 field 이외의 것 (ex. getter, setter 등) 도 체크하므로 해당 내용만 체크하도록 처리
+            if (node !is KotlinUField) return
+
+            val property = node.sourcePsi as? KtProperty
+            val propertyType = property?.typeReference ?: return
+            val propertyTypeName = propertyType.text
+            val isMutable = MutableNames.any { immutableName ->
+                propertyTypeName.startsWith(immutableName)
+            }
+
+            // mutable Collection 인 경우 && PUBLIC 인 경우
+            if (isMutable && property.isPublic) {
+                context.report(
+                    issue = MutableCollectionPublicIssue,
+                    scope = propertyType,
+                    location = context.getNameLocation(propertyType),
+                    message = Explanation,
+                )
+            }
+        }
+    }
+}
