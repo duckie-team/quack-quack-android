ee2b0640b1f92d32597f1822a26ab7ff68ddd32b
jisungbin <ji@sungb.in>
2022-08-23 08:00:19 +0900

    refactor: ShowkaseModule 에서 ShowkaseRoot 로 네이밍 변경

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/ShowkaseRoot.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/ShowkaseRoot.kt
@@ -15,4 +15,4 @@ import com.airbnb.android.showkase.annotation.ShowkaseRoot
 import com.airbnb.android.showkase.annotation.ShowkaseRootModule
 
 @ShowkaseRoot
-class ShowkaseModule : ShowkaseRootModule
+class ShowkaseRoot : ShowkaseRootModule
