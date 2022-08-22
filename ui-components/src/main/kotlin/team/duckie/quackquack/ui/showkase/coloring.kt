33807a75ffd826e6406bb2773037a333e891df61
jisungbin <ji@sungb.in>
2022-08-23 01:48:23 +0900

    feat: ShowkaseColor 리스트 추가

---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/showkase/coloring.kt
@@ -0,0 +1,25 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [coloring.kt] created by Ji Sungbin on 22. 8. 23. 오전 12:37
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.showkase
+
+import com.airbnb.android.showkase.annotation.ShowkaseColor
+import team.duckie.quackquack.ui.color.QuackColor
+
+@ShowkaseColor(name = "Orange", group = "DuckieColor")
+val QuackOrange = QuackColor.DuckieOrange.value
+
+@ShowkaseColor(name = "Gray", group = "DuckieColor")
+val QuackGray = QuackColor.Greyish.value
+
+@ShowkaseColor(name = "White", group = "DuckieColor")
+val QuackWhite = QuackColor.White.value
+
+@ShowkaseColor(name = "Black", group = "DuckieColor")
+val QuackBlack = QuackColor.Black.value
