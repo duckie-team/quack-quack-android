72171f50484e8fc701f5a2465334ee3144a5c3c4
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-24 20:30:49 +0900

    feat: TextFieldDefault 추가

---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/TextFieldDefaults.kt
@@ -0,0 +1,93 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [TextFieldDefaults.kt] created by Ji Sungbin on 22. 8. 21. 오후 5:48
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.constant
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import team.duckie.quackquack.ui.color.QuackColor
+
+@Stable
+interface QuackTextFieldColors {
+    @Composable
+    fun textColor(): QuackColor
+
+    @Composable
+    fun backgroundColor(): QuackColor
+
+    @Composable
+    fun placeholderColor(): QuackColor
+
+    @Composable
+    fun trailingIconColor(): QuackColor
+
+    @Composable
+    fun cursorColor(): QuackColor
+}
+
+@Immutable
+object QuackTextFieldDefaults {
+    internal val height = 44.dp
+    internal val width = 328.dp
+
+    private val horizontalPadding = 0.dp
+    private val topPadding = 18.dp
+    private val bottomPadding = 8.dp
+
+    @Composable
+    fun textFieldColors(
+        textColor: QuackColor = QuackColor.Black,
+        backgroundColor: QuackColor = QuackColor.White,
+        placeholderColor: QuackColor = QuackColor.Greyish,
+        trailingIconColor: QuackColor = QuackColor.Greyish,
+        cursorColor: QuackColor = QuackColor.DuckieOrange,
+    ): QuackTextFieldColors =
+        QuackDefaultTextFieldColors(
+            textColor = textColor,
+            backgroundColor = backgroundColor,
+            placeholderColor = placeholderColor,
+            trailingIconColor = trailingIconColor,
+            cursorColor = cursorColor,
+        )
+
+    @Composable
+    fun textFieldPadding(
+        start: Dp = horizontalPadding,
+        end: Dp = horizontalPadding,
+        top: Dp = topPadding,
+        bottom: Dp = bottomPadding
+    ): PaddingValues = PaddingValues(start, top, end, bottom)
+}
+
+@Immutable
+private class QuackDefaultTextFieldColors(
+    private val textColor: QuackColor,
+    private val backgroundColor: QuackColor,
+    private val placeholderColor: QuackColor,
+    private val trailingIconColor: QuackColor,
+    private val cursorColor: QuackColor
+) : QuackTextFieldColors {
+
+    @Composable
+    override fun textColor(): QuackColor = textColor
+
+    @Composable
+    override fun backgroundColor(): QuackColor = backgroundColor
+
+    @Composable
+    override fun placeholderColor(): QuackColor = placeholderColor
+
+    @Composable
+    override fun trailingIconColor(): QuackColor = trailingIconColor
+
+    @Composable
+    override fun cursorColor(): QuackColor = cursorColor
+}
