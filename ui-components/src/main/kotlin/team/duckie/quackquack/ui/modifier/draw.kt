a6617fceded4e3a5dccd7dc23f6b26689aeeaf1a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-26 02:20:27 +0900

    feat: bottom border 주는 Modifier 확장함수 추가

---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/modifier/draw.kt
@@ -0,0 +1,36 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [draw.kt] created by Ji Sungbin on 22. 8. 25. 오후 11:25
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.modifier
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.clipRect
+
+@Stable
+internal fun Modifier.bottomIndicatorLine(
+    indicatorBorder: BorderStroke
+): Modifier = drawWithContent {
+    drawContent()
+    clipRect {
+        val strokeWidth = indicatorBorder.width.value * density
+        val y = size.height
+        drawLine(
+            brush = indicatorBorder.brush,
+            strokeWidth = strokeWidth,
+            cap = StrokeCap.Square,
+            start = Offset.Zero.copy(y = y),
+            end = Offset(x = size.width, y = y)
+        )
+    }
+}
