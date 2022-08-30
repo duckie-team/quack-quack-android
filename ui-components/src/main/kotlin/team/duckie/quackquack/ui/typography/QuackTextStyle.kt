c2cd0d2f4641d897e1b377549be9db1f15df37bf
jisungbin <ji@sungb.in>
2022-08-31 05:32:49 +0900

    docs: QuackTextStyle 에 주석 추가

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/typography/QuackTextStyle.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/typography/QuackTextStyle.kt
@@ -1,7 +1,7 @@
 /*
  * Designed and developed by 2022 SungbinLand, Team Duckie
  *
- * [style.kt] created by Ji Sungbin on 22. 8. 14. 오후 7:37
+ * [QuackTextStyle.kt] created by Ji Sungbin on 22. 8. 14. 오후 7:37
  *
  * Licensed under the MIT.
  * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
@@ -23,7 +23,19 @@ import androidx.compose.ui.unit.sp
 import team.duckie.quackquack.ui.R
 import team.duckie.quackquack.ui.color.QuackColor
 
-// copy 사용을 방지하기 위해 data class 미사용
+/**
+ * 덕키에서 사용할 텍스트 스타일을 정의합니다. 추상화를 위해 컴포즈의 [TextStyle] 를 그대로 사용하는게 아닌
+ * 이 클래스를 사용해야 합니다.
+ *
+ * copy 를 통한 값 변경은 덕키 스타일 가이드의 텍스트 스타일 사전 정의가 깨짐으로
+ * copy 생성을 방지하기 위해 data class 가 아닌 class 가 사용됐습니다.
+ *
+ * @param color 텍스트 색상. 색상 역시 추상화를 위해 [QuackColor] 를 사용합니다.
+ * @param size 텍스트 한 글자 크기
+ * @param weight 텍스트 굵기
+ * @param letterSpacing 텍스트 자간
+ * @param lineHeight 텍스트 줄 크기
+ */
 @Immutable
 class QuackTextStyle private constructor(
     val color: QuackColor = QuackColor.Black,
@@ -32,11 +44,10 @@ class QuackTextStyle private constructor(
     val letterSpacing: TextUnit,
     val lineHeight: TextUnit,
 ) {
-    @Stable
     private val suit = FontFamily(Font(R.font.suit_variable))
 
     @Stable
-    internal fun toComposeStyle() = TextStyle(
+    internal fun asComposeStyle() = TextStyle(
         color = color.value,
         fontSize = size,
         fontFamily = suit,
@@ -47,7 +58,6 @@ class QuackTextStyle private constructor(
     )
 
     companion object {
-
         @Stable
         val HeadLine1 = QuackTextStyle(
             size = 20.sp,
@@ -113,7 +123,15 @@ class QuackTextStyle private constructor(
         )
     }
 
-    // TODO: 색상 변경 애니메이션 지원
+    /**
+     * 텍스트 스타일에서 색상 값만 변경이 필요할 때가 있습니다.
+     * 이를 대응하기 위해 현재 텍스트 스타일에서 텍스트 색상만 변경하여
+     * 새로운 텍스트 스타일을 반환합니다.
+     *
+     * @param newColor 변경할 색상
+     *
+     * @return 새로운 색상이 적용된 [QuackTextStyle]
+     */
     @Stable
     internal fun changeColor(
         newColor: QuackColor,
