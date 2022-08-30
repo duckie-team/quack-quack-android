e23a3f71fa4e962ae97e48e39bfe890bb8c97dd7
jisungbin <ji@sungb.in>
2022-08-31 08:19:47 +0900

    feat: Playground 기본 테마 설정

---
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/theme/color.kt
@@ -0,0 +1,72 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [color.kt] created by Ji Sungbin on 22. 8. 31. 오전 7:13
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.playground.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF954A00)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFFFDCC6)
+val md_theme_light_onPrimaryContainer = Color(0xFF301400)
+val md_theme_light_secondary = Color(0xFF755845)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFDCC6)
+val md_theme_light_onSecondaryContainer = Color(0xFF2B1708)
+val md_theme_light_tertiary = Color(0xFF5F6135)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFE5E6AE)
+val md_theme_light_onTertiaryContainer = Color(0xFF1C1D00)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF201A17)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF201A17)
+val md_theme_light_surfaceVariant = Color(0xFFF3DED3)
+val md_theme_light_onSurfaceVariant = Color(0xFF52443B)
+val md_theme_light_outline = Color(0xFF84746A)
+val md_theme_light_inverseOnSurface = Color(0xFFFBEEE8)
+val md_theme_light_inverseSurface = Color(0xFF362F2B)
+val md_theme_light_inversePrimary = Color(0xFFFFB785)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF954A00)
+
+val md_theme_dark_primary = Color(0xFFFFB785)
+val md_theme_dark_onPrimary = Color(0xFF502500)
+val md_theme_dark_primaryContainer = Color(0xFF713700)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDCC6)
+val md_theme_dark_secondary = Color(0xFFE4BFA8)
+val md_theme_dark_onSecondary = Color(0xFF422B1B)
+val md_theme_dark_secondaryContainer = Color(0xFF5B412F)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDCC6)
+val md_theme_dark_tertiary = Color(0xFFC8CA94)
+val md_theme_dark_onTertiary = Color(0xFF31320B)
+val md_theme_dark_tertiaryContainer = Color(0xFF47491F)
+val md_theme_dark_onTertiaryContainer = Color(0xFFE5E6AE)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF201A17)
+val md_theme_dark_onBackground = Color(0xFFECE0DA)
+val md_theme_dark_surface = Color(0xFF201A17)
+val md_theme_dark_onSurface = Color(0xFFECE0DA)
+val md_theme_dark_surfaceVariant = Color(0xFF52443B)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD7C3B7)
+val md_theme_dark_outline = Color(0xFF9F8D83)
+val md_theme_dark_inverseOnSurface = Color(0xFF201A17)
+val md_theme_dark_inverseSurface = Color(0xFFECE0DA)
+val md_theme_dark_inversePrimary = Color(0xFF954A00)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFFFB785)
+
+val seed = Color(0xFFFF8300)
