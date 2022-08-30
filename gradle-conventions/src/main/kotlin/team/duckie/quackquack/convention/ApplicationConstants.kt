a38c2e0ca0d056799be0e07d13a6936d058e7fd8
jisungbin <ji@sungb.in>
2022-08-31 08:16:15 +0900

    chore: minSdk 23 으로 변경
    SystemUiController 가 SDK 23 이상을 요구함

--- gradle-conventions/src/main/kotlin/team/duckie/quackquack/convention/ApplicationConstants.kt
+++ gradle-conventions/src/main/kotlin/team/duckie/quackquack/convention/ApplicationConstants.kt
@@ -15,7 +15,7 @@ import org.gradle.api.JavaVersion
  * 그레이들 설정에 사용될 기본 상수 값들 모음
  */
 internal object ApplicationConstants {
-    const val minSdk = 21
+    const val minSdk = 23
     const val targetSdk = 33
     const val compileSdk = 33
     const val jvmTarget = "11"
