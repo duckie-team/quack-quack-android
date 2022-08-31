1b5710eb9257bad5341958b24b67b221f6908bcb
jisungbin <ji@sungb.in>
2022-08-31 17:28:47 +0900

    feat: detekt 규칙 수정

---
+++ common/src/main/kotlin/team/duckie/quackquack/common/detekt.kt
@@ -0,0 +1,13 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [detekt.kt] created by Ji Sungbin on 22. 8. 31. 오후 5:12
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.common
+
+@Retention(AnnotationRetention.SOURCE)
+annotation class AllowMagicNumber
