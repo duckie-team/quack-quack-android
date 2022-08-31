b01f45427dd502df1d2ed2f7d9971d7780a2a7ee
EvergreenTree97 <tkdfhr7497@naver.com>
2022-09-01 01:04:49 +0900

    fix: 필요없는 함수 제거

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/TextFieldDefaults.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/TextFieldDefaults.kt
@@ -11,7 +11,10 @@ package team.duckie.quackquack.ui.constant
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
@@ -68,12 +71,6 @@ object QuackTextFieldDefaults {
         @Composable
         @ReadOnlyComposable
         get() = RectangleShape
-
-    internal val InnerTextFieldShape: Shape
-        @Composable
-        @ReadOnlyComposable
-        get() = SmallShape
-
     @Composable
     internal fun textFieldColors(
         textColor: QuackColor = QuackColor.Black,
