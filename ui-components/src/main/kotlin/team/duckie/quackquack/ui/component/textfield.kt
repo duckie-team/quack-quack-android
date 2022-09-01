25db0f696f7714fe5c8b55ff7019b3497220d654
EvergreenTree97 <tkdfhr7497@naver.com>
2022-09-01 15:49:43 +0900

    feat: TextField, TextFieldDefaults

---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/textfield.kt
@@ -0,0 +1,447 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [textfield.kt] created by EvergreenTree97 on 22. 8. 21. 오후 3:26
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import team.duckie.quackquack.ui.color.QuackColor
+import team.duckie.quackquack.ui.constant.QuackHeight
+import team.duckie.quackquack.ui.constant.QuackTextFieldColors
+import team.duckie.quackquack.ui.constant.QuackTextFieldDefaults
+import team.duckie.quackquack.ui.constant.QuackWidth
+import team.duckie.quackquack.ui.icon.QuackIcon
+import team.duckie.quackquack.ui.modifier.applyQuackSize
+import team.duckie.quackquack.ui.modifier.bottomIndicatorLine
+import team.duckie.quackquack.ui.typography.QuackBody1
+import team.duckie.quackquack.ui.typography.QuackSubtitle
+import team.duckie.quackquack.ui.typography.QuackTextStyle
+
+@Composable
+fun QuackAddSendTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    leadingIcon: QuackIcon,
+    trailingIcon: QuackIcon,
+    onLeadingClick: () -> Unit,
+    onTrailingClick: () -> Unit,
+) {
+    QuackBasicTextField(
+        text = text, onTextChanged = onTextChanged,
+        leading = {
+            QuackSimpleIcon(
+                icon = leadingIcon,
+                onClick = onLeadingClick,
+            )
+        },
+        trailing = {
+            QuackSimpleIcon(
+                icon = trailingIcon,
+                tint = QuackTextFieldDefaults.textFieldColors().trailingIconColor(
+                    isFocused = text.isNotEmpty()
+                ),
+                onClick = onTrailingClick,
+            )
+        },
+        margin = PaddingValues(
+            horizontal = QuackTextFieldDefaults.iconHorizontalPadding,
+            vertical = QuackTextFieldDefaults.iconVerticalPadding,
+        ),
+    )
+}
+
+@Composable
+fun QuackLimitedTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    placeholder: String,
+    isError: Boolean,
+    errorText: String,
+    maxLength: Int,
+    icon: QuackIcon = QuackIcon.Delete,
+    onClickButton: () -> Unit,
+) {
+    QuackBasicTextField(
+        text = text,
+        onTextChanged = {
+            if (it.length <= maxLength) {
+                onTextChanged(it)
+            }
+        },
+        placeholder = placeholder,
+        isError = isError,
+        errorText = errorText,
+        trailing = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                QuackBody1(
+                    text = "${text.length}",
+                    color = QuackTextFieldDefaults.textFieldColors().textColor(
+                        isFocused = text.isNotEmpty()
+                    )
+                )
+                Spacer(
+                    modifier = Modifier.width(
+                        width = QuackTextFieldDefaults.lengthTextSpacing,
+                    ),
+                )
+                QuackBody1(
+                    text = "/",
+                    color = QuackColor.Gray2,
+                )
+                Spacer(
+                    modifier = Modifier.width(
+                        width = QuackTextFieldDefaults.lengthTextSpacing,
+                    ),
+                )
+                QuackBody1(
+                    text = "$maxLength",
+                    color = QuackColor.Gray2,
+                )
+                if (text.isNotEmpty()) {
+                    Spacer(
+                        modifier = Modifier.width(
+                            width = QuackTextFieldDefaults.smallIconSpacing,
+                        )
+                    )
+                    QuackSimpleIcon(
+                        icon = icon,
+                        onClick = onClickButton,
+                    )
+                }
+            }
+        },
+        style = QuackTextStyle.Title1
+    )
+}
+
+@Composable
+fun QuackTrailingTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    buttonText: String,
+    placeholder: String,
+    onClickButton: () -> Unit,
+) {
+    QuackBasicTextField(
+        text = text,
+        onTextChanged = onTextChanged,
+        placeholder = placeholder,
+        keyboardActions = KeyboardActions(
+            onDone = {
+                onClickButton()
+            },
+        ),
+        trailing = {
+            QuackSubtitle(
+                modifier = Modifier.clickable(text.isNotEmpty()) {
+                    onClickButton()
+                },
+                text = buttonText,
+                color = QuackTextFieldDefaults.textFieldColors().trailingIconColor(
+                    isFocused = text.isNotEmpty()
+                ),
+            )
+        },
+    )
+}
+
+@Composable
+fun QuackIconTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    icon: QuackIcon,
+    placeholder: String,
+) {
+    QuackBasicTextField(
+        text = text,
+        onTextChanged = onTextChanged,
+        placeholder = placeholder,
+        iconSpacing = when (icon) {
+            QuackIcon.Won -> QuackTextFieldDefaults.smallIconSpacing
+            else -> QuackTextFieldDefaults.iconSpacing
+        },
+        leading = {
+            QuackSimpleIcon(
+                icon = icon,
+                tint = QuackTextFieldDefaults.textFieldColors().textColor(
+                    isFocused = text.isNotEmpty()
+                ),
+            )
+        },
+    )
+}
+
+@Composable
+fun QuackTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    placeholder: String,
+) {
+    QuackBasicTextField(
+        text = text,
+        onTextChanged = onTextChanged,
+        placeholder = placeholder,
+    )
+}
+
+@Composable
+internal fun QuackBasicTextField(
+    text: String,
+    onTextChanged: (text: String) -> Unit,
+    placeholder: String = "",
+    width: QuackWidth = QuackWidth.Fill,
+    height: QuackHeight = QuackHeight.Custom(QuackTextFieldDefaults.height),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    trailing: @Composable (() -> Unit)? = null,
+    leading: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    errorText: String = "",
+    margin: PaddingValues = QuackTextFieldDefaults.textFieldPadding(),
+    iconSpacing: Dp = QuackTextFieldDefaults.iconSpacing,
+    singleLine: Boolean = true,
+    colors: QuackTextFieldColors = QuackTextFieldDefaults.textFieldColors(),
+    shape: Shape = QuackTextFieldDefaults.TextFieldShape,
+    style: QuackTextStyle = QuackTextStyle.Body1,
+) {
+    BasicTextField(
+        modifier = Modifier.testTag(
+            tag = "TextFieldTag"
+        ),
+        value = text,
+        onValueChange = onTextChanged,
+        textStyle = style.changeTextAlign(TextAlign.Left).asComposeStyle(),
+        cursorBrush = SolidColor(
+            value = colors.cursorColor().value,
+        ),
+        singleLine = singleLine,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        decorationBox = { innerTextField ->
+            QuackCommonDecorationBox(
+                text = text,
+                placeholder = placeholder,
+                width = width,
+                height = height,
+                trailing = trailing,
+                icon = leading,
+                isError = isError,
+                margin = margin,
+                errorText = errorText,
+                iconSpacing = iconSpacing,
+                innerTextField = innerTextField,
+                shape = shape,
+                colors = colors,
+            )
+        },
+    )
+}
+
+@Composable
+private fun QuackCommonDecorationBox(
+    text: String,
+    width: QuackWidth,
+    height: QuackHeight,
+    innerTextField: @Composable () -> Unit,
+    trailing: @Composable (() -> Unit)?,
+    icon: @Composable (() -> Unit)?,
+    isError: Boolean,
+    errorText: String,
+    placeholder: String,
+    margin: PaddingValues,
+    iconSpacing: Dp,
+    colors: QuackTextFieldColors,
+    shape: Shape,
+) {
+    Column {
+        QuackSurface(
+            modifier = Modifier
+                .applyQuackSize(
+                    width = width,
+                    height = height,
+                )
+                .clip(
+                    shape = shape,
+                )
+                .bottomIndicatorLine(
+                    QuackTextFieldDefaults.indicatorStroke(
+                        isError = isError,
+                    )
+                ),
+            contentAlignment = Alignment.CenterStart,
+            backgroundColor = colors.backgroundColor(),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        paddingValues = margin,
+                    ),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Row(
+                    modifier = Modifier.weight(
+                        weight = 1f,
+                    ), verticalAlignment = Alignment.CenterVertically
+                ) {
+                    icon?.run {
+                        this()
+                        Spacer(
+                            modifier = Modifier.width(
+                                width = iconSpacing,
+                            ),
+                        )
+                    }
+                    Box {
+                        if (text.isEmpty()) {
+                            QuackBody1(
+                                text = placeholder,
+                                color = colors.placeholderColor(),
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
+                trailing?.run {
+                    Spacer(
+                        modifier = Modifier.width(
+                            iconSpacing,
+                        )
+                    )
+                    this()
+                }
+            }
+        }
+        if (isError) {
+            Spacer(
+                modifier = Modifier.height(
+                    QuackTextFieldDefaults.errorTextSpacing,
+                ),
+            )
+            QuackBody1(
+                text = errorText,
+                color = colors.errorTextColor(),
+            )
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun QuackTextFieldPreview() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val placeholder = "placeholder"
+    QuackTextField(
+        text = text,
+        onTextChanged = setText,
+        placeholder = placeholder,
+    )
+}
+
+@Composable
+@Preview
+fun QuackIconTextFieldPreview() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val placeholder = "placeholder"
+    val placeholder2 = "가격(필수 입력)"
+    Column {
+        QuackIconTextField(
+            text = text,
+            onTextChanged = setText,
+            icon = QuackIcon.Search,
+            placeholder = placeholder,
+        )
+        Spacer(
+            modifier = Modifier.height(
+                height = 20.dp,
+            ),
+        )
+        QuackIconTextField(
+            text = text,
+            onTextChanged = setText,
+            icon = QuackIcon.Won,
+            placeholder = placeholder2,
+        )
+    }
+
+}
+
+@Composable
+@Preview
+fun QuackTrailingTextFieldPreview() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val placeholder = "placeholder"
+    val buttonText = "등록"
+    QuackTrailingTextField(
+        text = text,
+        onTextChanged = setText,
+        buttonText = buttonText,
+        placeholder = placeholder,
+        onClickButton = {},
+    )
+}
+
+private const val MaxLength = 30
+
+@Composable
+@Preview
+fun QuackLimitedTextFieldPreview() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val placeholder = "placeholder"
+    val errorText = "특수문자가 포함되어 있습니다"
+    val maxLength = MaxLength
+    val isError = text.endsWith("E")
+    QuackLimitedTextField(text = text,
+        onTextChanged = setText,
+        placeholder = placeholder,
+        errorText = errorText,
+        isError = isError,
+        maxLength = maxLength,
+        onClickButton = {})
+}
+
+@Composable
+@Preview
+fun QuackAddSendTextFieldPreview() {
+    val (text, setText) = remember { mutableStateOf("") }
+    QuackAddSendTextField(text = text,
+        onTextChanged = setText,
+        leadingIcon = QuackIcon.Plus,
+        trailingIcon = QuackIcon.ArrowSend,
+        onTrailingClick = {},
+        onLeadingClick = {})
+}
---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/constant/textfieldDefaults.kt
@@ -0,0 +1,155 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [textfieldDefaults.kt] created by EvergreenTree97 on 22. 8. 21. 오후 5:48
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.constant
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import team.duckie.quackquack.ui.color.QuackColor
+
+@Immutable
+object QuackTextFieldDefaults {
+    internal val height = 44.dp
+
+    internal val iconSpacing = 8.dp
+    internal val smallIconSpacing = 4.dp
+    internal val errorTextSpacing = 4.dp
+    internal val lengthTextSpacing = 2.dp
+
+    internal val iconHorizontalPadding = 12.dp
+    internal val iconVerticalPadding = 14.dp
+
+    private val horizontalPadding = 0.dp
+    private val topPadding = 18.dp
+    private val bottomPadding = 8.dp
+
+    private val errorIndicatorThickness = 1.dp
+
+    internal val TextFieldShape: Shape
+        @Composable
+        @ReadOnlyComposable
+        get() = RectangleShape
+
+    @Composable
+    internal fun textFieldColors(
+        textColor: QuackColor = QuackColor.Black,
+        unFocusedColor: QuackColor = QuackColor.Gray2,
+        errorTextColor: QuackColor = QuackColor.OrangeRed,
+        backgroundColor: QuackColor = QuackColor.White,
+        placeholderColor: QuackColor = QuackColor.Gray2,
+        trailingIconColor: QuackColor = QuackColor.DuckieOrange,
+        unFocusedTrailingIconColor: QuackColor = QuackColor.Gray2,
+        cursorColor: QuackColor = QuackColor.DuckieOrange,
+        indicatorColor: QuackColor = QuackColor.Unspecified,
+        errorIndicatorColor: QuackColor = QuackColor.OrangeRed,
+    ): QuackTextFieldColors =
+        QuackTextFieldColors(
+            textColor = textColor,
+            unFocusedTextColor = unFocusedColor,
+            errorTextColor = errorTextColor,
+            backgroundColor = backgroundColor,
+            placeholderColor = placeholderColor,
+            trailingIconColor = trailingIconColor,
+            unFocusedTrailingIconColor = unFocusedTrailingIconColor,
+            cursorColor = cursorColor,
+            indicatorColor = indicatorColor,
+            errorIndicatorColor = errorIndicatorColor,
+        )
+
+    @Composable
+    internal fun textFieldPadding(
+        start: Dp = horizontalPadding,
+        end: Dp = horizontalPadding,
+        top: Dp = topPadding,
+        bottom: Dp = bottomPadding,
+    ): PaddingValues = PaddingValues(
+        start = start,
+        top = top,
+        end = end,
+        bottom = bottom,
+    )
+
+    @Composable
+    internal fun indicatorStroke(
+        width: Dp = errorIndicatorThickness,
+        colors: QuackTextFieldColors = textFieldColors(),
+        isError: Boolean,
+    ): BorderStroke = BorderStroke(
+        width = width,
+        color = colors.indicatorColor(
+            isError = isError
+        ).value
+    )
+}
+
+@Immutable
+internal class QuackTextFieldColors(
+    private val textColor: QuackColor,
+    private val unFocusedTextColor: QuackColor,
+    private val errorTextColor: QuackColor,
+    private val placeholderColor: QuackColor,
+    private val trailingIconColor: QuackColor,
+    private val unFocusedTrailingIconColor: QuackColor,
+    private val backgroundColor: QuackColor,
+    private val cursorColor: QuackColor,
+    private val indicatorColor: QuackColor,
+    private val errorIndicatorColor: QuackColor,
+) {
+    @Composable
+    fun textColor(
+        isFocused: Boolean,
+    ): QuackColor {
+        return if (isFocused) {
+            textColor
+        } else {
+            unFocusedTextColor
+        }
+    }
+
+    @Composable
+    fun errorTextColor() = errorTextColor
+
+    @Composable
+    fun placeholderColor() = placeholderColor
+
+    @Composable
+    fun trailingIconColor(
+        isFocused: Boolean = false,
+    ): QuackColor {
+        return if (isFocused) {
+            trailingIconColor
+        } else {
+            unFocusedTrailingIconColor
+        }
+    }
+
+    @Composable
+    fun backgroundColor() = backgroundColor
+
+    @Composable
+    fun cursorColor() = cursorColor
+
+    @Composable
+    fun indicatorColor(
+        isError: Boolean,
+    ): QuackColor {
+        return if (isError) {
+            errorIndicatorColor
+        } else {
+            indicatorColor
+        }
+    }
+}
