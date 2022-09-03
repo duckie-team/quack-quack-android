b1e8475e5733744c8880eb447520e3550e4ee3af
jisungbin <ji@sungb.in>
2022-09-03 22:37:45 +0900

    refactor: detekt 규칙에 맞게 수정

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/textfield.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/textfield.kt
@@ -17,9 +17,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.NonRestartableComposable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,39 +29,36 @@ import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import team.duckie.quackquack.common.DoNotUseDirectly
 import team.duckie.quackquack.common.npe
-import team.duckie.quackquack.ui.color.QuackColor
 import team.duckie.quackquack.ui.constant.QuackHeight
 import team.duckie.quackquack.ui.constant.QuackWidth
 import team.duckie.quackquack.ui.modifier.applyQuackSize
 import team.duckie.quackquack.ui.textstyle.QuackTextStyle
 
-/**
- * QuackTextField 에서 표시할 텍스트의 [FontWeight] 에 따라 QuackTextField 의
- * 높이가 달라집니다. 이를 계산하기 위해 사용됩니다.
- *
- * 만약 [FontWeight] 가 [FontWeight.Bold] 라면 TextField 에서 16dp 만큼 추가 높이를 갖고,
- * 그렇지 않다면 18dp 만큼 추가 높이를 갖습니다.
- *
- * 좀 더 많은 경우를 신경쓰기 위해선 [FontWeight.Bold] 이상인 경우도 체크하면 좋지만,
- * 현재 QuackTextField 의 디자인에는 최대 [FontWeight.Bold] 만 사용하고 있기 때문에
- * [FontWeight.Bold] 만 체크합니다.
- *
- * @receiver [FontWeight] 가 [FontWeight.Bold] 인지 검사할 [QuackTextStyle]
- * @return QuackTextField 에서 표시할 텍스트의 [FontWeight] 가 [FontWeight.Bold] 인지 여부
- */
-private val QuackTextStyle.isBold get() = weight == FontWeight.Bold
-
-/**
- * QuackTextField 밑에 표시될 ErrorText 와의 간격
- */
-private val QuackTextFieldErrorTextTopPadding = 4.dp
+// /**
+//  * QuackTextField 에서 표시할 텍스트의 [FontWeight] 에 따라 QuackTextField 의
+//  * 높이가 달라집니다. 이를 계산하기 위해 사용됩니다.
+//  *
+//  * 만약 [FontWeight] 가 [FontWeight.Bold] 라면 TextField 에서 16dp 만큼 추가 높이를 갖고,
+//  * 그렇지 않다면 18dp 만큼 추가 높이를 갖습니다.
+//  *
+//  * 좀 더 많은 경우를 신경쓰기 위해선 [FontWeight.Bold] 이상인 경우도 체크하면 좋지만,
+//  * 현재 QuackTextField 의 디자인에는 최대 [FontWeight.Bold] 만 사용하고 있기 때문에
+//  * [FontWeight.Bold] 만 체크합니다.
+//  *
+//  * @receiver [FontWeight] 가 [FontWeight.Bold] 인지 검사할 [QuackTextStyle]
+//  * @return QuackTextField 에서 표시할 텍스트의 [FontWeight] 가 [FontWeight.Bold] 인지 여부
+//  */
+// private val QuackTextStyle.isBold get() = weight == FontWeight.Bold
+//
+// /**
+//  * QuackTextField 밑에 표시될 ErrorText 와의 간격
+//  */
+// private val QuackTextFieldErrorTextTopPadding = 4.dp
 
 /**
  * TextField 양옆에 추가되는 Decoration 들과의 간격
@@ -76,144 +71,144 @@ private val QuackTextFieldErrorTextTopPadding = 4.dp
  */
 private val QuackTextFieldDecorationContentHorizontalPadding = 8.dp
 
-/**
- * QuackTextField 에서 표시하는 텍스트의 [FontWeight] 가
- * [FontWeight.Bold] 가 아닐 때 TextField 위에 들어갈 패딩
- *
- * QuackTextField 의 높이를 결정짓는 중요한 요소가 됩니다.
- *
- * **이 API 는 직접적으로 사용하면 안됩니다.**
- * 대신에 [QuackTextStyle.calcQuackTextFieldTopPadding] 를 사용하세요.
- *
- * @see QuackTextStyle.isBold
- */
-@DoNotUseDirectly
-private val QuackTextFieldTopPaddingWithNonBold = 18.dp
+// /**
+//  * QuackTextField 에서 표시하는 텍스트의 [FontWeight] 가
+//  * [FontWeight.Bold] 가 아닐 때 TextField 위에 들어갈 패딩
+//  *
+//  * QuackTextField 의 높이를 결정짓는 중요한 요소가 됩니다.
+//  *
+//  * **이 API 는 직접적으로 사용하면 안됩니다.**
+//  * 대신에 [QuackTextStyle.calcQuackTextFieldTopPadding] 를 사용하세요.
+//  *
+//  * @see QuackTextStyle.isBold
+//  */
+// @DoNotUseDirectly
+// private val QuackTextFieldTopPaddingWithNonBold = 18.dp
 
-/**
- * QuackTextField 에서 표시하는 텍스트의 [FontWeight] 가
- * [FontWeight.Bold] 일 때 TextField 위에 들어갈 패딩
- *
- * QuackTextField 의 높이를 결정짓는 중요한 요소가 됩니다.
- *
- * **이 API 는 직접적으로 사용하면 안됩니다.**
- * 대신에 [QuackTextStyle.calcQuackTextFieldTopPadding] 를 사용하세요.
- *
- * @see QuackTextStyle.isBold
- */
-@DoNotUseDirectly
-private val QuackTextFieldTopPaddingWithBold = 16.dp
+// /**
+//  * QuackTextField 에서 표시하는 텍스트의 [FontWeight] 가
+//  * [FontWeight.Bold] 일 때 TextField 위에 들어갈 패딩
+//  *
+//  * QuackTextField 의 높이를 결정짓는 중요한 요소가 됩니다.
+//  *
+//  * **이 API 는 직접적으로 사용하면 안됩니다.**
+//  * 대신에 [QuackTextStyle.calcQuackTextFieldTopPadding] 를 사용하세요.
+//  *
+//  * @see QuackTextStyle.isBold
+//  */
+// @DoNotUseDirectly
+// private val QuackTextFieldTopPaddingWithBold = 16.dp
 
-@OptIn(DoNotUseDirectly::class)
-private fun QuackTextStyle.calcQuackTextFieldTopPadding() = when (isBold) {
-    true -> QuackTextFieldTopPaddingWithBold
-    else -> QuackTextFieldTopPaddingWithNonBold
-}
+// OptIn(DoNotUseDirectly::class)
+// private fun QuackTextStyle.calcQuackTextFieldTopPadding() = when (isBold) {
+//    true -> QuackTextFieldTopPaddingWithBold
+//    else -> QuackTextFieldTopPaddingWithNonBold
+// }
 
-/**
- * QuackTextField 에 사용되는 TextField 의 하단에 들어갈 패딩
- */
-private val QuackTextFieldBottomPadding = 8.dp
+// /**
+//  * QuackTextField 에 사용되는 TextField 의 하단에 들어갈 패딩
+//  */
+// private val QuackTextFieldBottomPadding = 8.dp
 
-/**
- * QuackTextField 에 표시될 언더바의 높이
- */
-private val QuackTextFieldUnderBarHeight = 1.dp
+// /**
+//  * QuackTextField 에 표시될 언더바의 높이
+//  */
+// private val QuackTextFieldUnderBarHeight = 1.dp
 
-/**
- * QuackTextField 에서 사용할 아이템들의 색상을
- * 계산하기 위한 유틸 함수들이 있는 클래스 입니다.
- */
-@Immutable
-private object QuackTextFieldColors {
-    /**
-     * QuackTextField 에 표시될 텍스트의 색상을 계산합니다.
-     * placeholder 인지 여부에 따라 색상이 달라집니다.
-     *
-     * @param isPlaceholder placeholder 로 보여지고 있는지 여부
-     * @return QuackTextField 에 표시될 텍스트의 색상
-     */
-    @Stable
-    fun textColor(
-        isPlaceholder: Boolean,
-    ) = when (isPlaceholder) {
-        true -> QuackColor.Black
-        else -> QuackColor.Gray2
-    }
-
-    /**
-     * QuackTextField 에 표시될 후행 텍스트의 색상을 계산합니다.
-     * placeholder 인지 여부에 따라 색상이 달라집니다.
-     *
-     * @param isPlaceholder placeholder 로 보여지고 있는지 여부
-     * @return QuackTextField 에 표시될 후행 텍스트의 색상
-     */
-    @Stable
-    fun trailingTextColor(
-        isPlaceholder: Boolean = false,
-    ) = when (isPlaceholder) {
-        true -> QuackColor.DuckieOrange
-        else -> textColor(
-            isPlaceholder = false,
-        )
-    }
-
-    /**
-     * QuackTextField 의 후행으로 붙는 아이템의 색상을 정의합니다.
-     *
-     * QuackTextField 디자인에서 후행 아이템의 색상은
-     * placeholder 인 textColor 색상을 그대로 사용하므로
-     * [textColor] 를 그대로 델리게이트 합니다.
-     *
-     * QuackTextField 의 후행으로 올 수 있는 아이템은
-     * 아이콘 의외에 텍스트도 있으므로 Icon 이 아닌 Decorator
-     * 단어를 사용하였습니다.
-     */
-    val trailingDecoratorColor = textColor(
-        isPlaceholder = false,
-    )
-
-    /**
-     * QuackTextField 에 표시될 선행 아이콘의 색상을 계산합니다.
-     * placeholder 인지 여부에 따라 색상이 달라집니다.
-     *
-     * QuackTextField 디자인에서 선행 아이템의 색상은
-     * QuackTextField 의 텍스트 색상을 그대로 사용하므로
-     * [textColor] 를 그대로 델리게이트 합니다.
-     *
-     * @param isPlaceholder placeholder 로 보여지고 있는지 여부
-     * @return QuackTextField 에 표시될 선행 아이콘의 색상
-     */
-    @Stable
-    fun leadingIconColor(
-        isPlaceholder: Boolean,
-    ) = textColor(
-        isPlaceholder = isPlaceholder,
-    )
-
-    /**
-     * QuackTextField 에 표시될 언더바의 색상을 계산합니다.
-     * 현재 에러 상태인지에 따라 색상이 달라집니다.
-     *
-     * @param isError 현재 QuackTextField 가 에러 상태인지 여부
-     * @return QuackTextField 에 표시될 언더바의 색상
-     */
-    @Stable
-    fun underBarColor(
-        isError: Boolean,
-    ) = when (isError) {
-        true -> QuackColor.OrangeRed
-        else -> QuackColor.Gray3
-    }
-
-    /**
-     * QuackTextField 에서 사용할 커서 색상을 정의합니다.
-     * QuackTextField 는 커서의 색상으로 항상 [QuackColor.DuckieOrange]
-     * 를 사용합니다.
-     */
-    @Stable
-    val QuackTextFieldCursorColor = QuackColor.DuckieOrange
-}
+// /**
+//  * QuackTextField 에서 사용할 아이템들의 색상을
+//  * 계산하기 위한 유틸 함수들이 있는 클래스 입니다.
+//  */
+// @Immutable
+// private object QuackTextFieldColors {
+//     /**
+//      * QuackTextField 에 표시될 텍스트의 색상을 계산합니다.
+//      * placeholder 인지 여부에 따라 색상이 달라집니다.
+//      *
+//      * @param isPlaceholder placeholder 로 보여지고 있는지 여부
+//      * @return QuackTextField 에 표시될 텍스트의 색상
+//      */
+//     @Stable
+//     fun textColor(
+//         isPlaceholder: Boolean,
+//     ) = when (isPlaceholder) {
+//         true -> QuackColor.Black
+//         else -> QuackColor.Gray2
+//     }
+//
+//     /**
+//      * QuackTextField 에 표시될 후행 텍스트의 색상을 계산합니다.
+//      * placeholder 인지 여부에 따라 색상이 달라집니다.
+//      *
+//      * @param isPlaceholder placeholder 로 보여지고 있는지 여부
+//      * @return QuackTextField 에 표시될 후행 텍스트의 색상
+//      */
+//     @Stable
+//     fun trailingTextColor(
+//         isPlaceholder: Boolean = false,
+//     ) = when (isPlaceholder) {
+//         true -> QuackColor.DuckieOrange
+//         else -> textColor(
+//             isPlaceholder = false,
+//         )
+//     }
+//
+//     /**
+//      * QuackTextField 의 후행으로 붙는 아이템의 색상을 정의합니다.
+//      *
+//      * QuackTextField 디자인에서 후행 아이템의 색상은
+//      * placeholder 인 textColor 색상을 그대로 사용하므로
+//      * [textColor] 를 그대로 델리게이트 합니다.
+//      *
+//      * QuackTextField 의 후행으로 올 수 있는 아이템은
+//      * 아이콘 의외에 텍스트도 있으므로 Icon 이 아닌 Decorator
+//      * 단어를 사용하였습니다.
+//      */
+//     val trailingDecoratorColor = textColor(
+//         isPlaceholder = false,
+//     )
+//
+//     /**
+//      * QuackTextField 에 표시될 선행 아이콘의 색상을 계산합니다.
+//      * placeholder 인지 여부에 따라 색상이 달라집니다.
+//      *
+//      * QuackTextField 디자인에서 선행 아이템의 색상은
+//      * QuackTextField 의 텍스트 색상을 그대로 사용하므로
+//      * [textColor] 를 그대로 델리게이트 합니다.
+//      *
+//      * @param isPlaceholder placeholder 로 보여지고 있는지 여부
+//      * @return QuackTextField 에 표시될 선행 아이콘의 색상
+//      */
+//     @Stable
+//     fun leadingIconColor(
+//         isPlaceholder: Boolean,
+//     ) = textColor(
+//         isPlaceholder = isPlaceholder,
+//     )
+//
+//     /**
+//      * QuackTextField 에 표시될 언더바의 색상을 계산합니다.
+//      * 현재 에러 상태인지에 따라 색상이 달라집니다.
+//      *
+//      * @param isError 현재 QuackTextField 가 에러 상태인지 여부
+//      * @return QuackTextField 에 표시될 언더바의 색상
+//      */
+//     @Stable
+//     fun underBarColor(
+//         isError: Boolean,
+//     ) = when (isError) {
+//         true -> QuackColor.OrangeRed
+//         else -> QuackColor.Gray3
+//     }
+//
+//     /**
+//      * QuackTextField 에서 사용할 커서 색상을 정의합니다.
+//      * QuackTextField 는 커서의 색상으로 항상 [QuackColor.DuckieOrange]
+//      * 를 사용합니다.
+//      */
+//     @Stable
+//     val QuackTextFieldCursorColor = QuackColor.DuckieOrange
+// }
 
 @Composable
 @NonRestartableComposable
