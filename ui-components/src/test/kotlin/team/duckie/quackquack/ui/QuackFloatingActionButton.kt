b3c70895cf362a2a76c1523a176e429365d85979
EvergreenTree97 <tkdfhr7497@naver.com>
2022-09-04 14:01:25 +0900

    test: 스냅샷 테스트

--- ui-components/src/test/kotlin/team/duckie/quackquack/ui/QuackFloatingActionButton.kt
+++ ui-components/src/test/kotlin/team/duckie/quackquack/ui/QuackFloatingActionButton.kt
@@ -13,16 +13,19 @@ package team.duckie.quackquack.ui
 
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import team.duckie.quackquack.ui.component.QuackPopUpMenuItem
+import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.rule.AnimationTestRule
 import team.duckie.quackquack.ui.textstyle.QuackFontScale
 import team.duckie.quackquack.ui.util.boxSnapshot
 import team.duckie.quackquack.ui.util.buildPaparazzi
 
 @RunWith(TestParameterInjector::class)
-class QuackFloatingActionButton {
+class QuackFloatingActionButtonSnapShot {
     @get:Rule
     val paparazzi = buildPaparazzi()
 
@@ -37,9 +40,36 @@ class QuackFloatingActionButton {
             name = "[fontScale:$fontScale]",
         ) {
             QuackFontScale = fontScale
-            team.duckie.quackquack.ui.component.FloatingActionButton(
+            team.duckie.quackquack.ui.component.QuackFloatingActionButton(
+                icon = QuackIcon.DMNew,
                 onClick = {},
             )
         }
     }
+
+    @Test
+    fun QuackMenuFloatingActionButton(
+        @TestParameter("0.5", "1.0", "1.5", "2.0") fontScale: Double,
+    ) {
+        paparazzi.boxSnapshot(
+            name = "[fontScale:$fontScale]",
+        ) {
+            QuackFontScale = fontScale
+            team.duckie.quackquack.ui.component.QuackMenuFloatingActionButton(
+                items = persistentListOf(
+                    QuackPopUpMenuItem(
+                        quackIcon = QuackIcon.WriteFeed,
+                        text = "피드",
+                        onClick = {}
+                    ),
+                    QuackPopUpMenuItem(
+                        quackIcon = QuackIcon.DrawerBuy,
+                        text = "덕딜",
+                        onClick = {}
+                    ),
+                )
+            )
+        }
+    }
 }
+
