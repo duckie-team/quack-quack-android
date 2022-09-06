17c816ee8b1d4ac7a6adbc320c9b9396fcd397e2
EvergreenTree97 <tkdfhr7497@naver.com>
2022-09-06 17:50:18 +0900

    test: 스냅샷 테스트 expanded parameter로 제공

--- ui-components/src/test/kotlin/team/duckie/quackquack/ui/QuackFab.kt
+++ ui-components/src/test/kotlin/team/duckie/quackquack/ui/QuackFab.kt
@@ -21,13 +21,18 @@ import team.duckie.quackquack.ui.component.QuackPopUpMenuItem
 import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.rule.AnimationTestRule
 import team.duckie.quackquack.ui.textstyle.QuackFontScale
+import team.duckie.quackquack.ui.util.PaparazziConfig
 import team.duckie.quackquack.ui.util.boxSnapshot
 import team.duckie.quackquack.ui.util.buildPaparazzi
 
 @RunWith(TestParameterInjector::class)
 class QuackFab {
     @get:Rule
-    val paparazzi = buildPaparazzi()
+    val paparazzi = buildPaparazzi {
+        this.apply {
+            screenHeight = 500
+        }
+    }
 
     @get:Rule
     val animationTest = AnimationTestRule()
@@ -49,21 +54,25 @@ class QuackFab {
 
     @Test
     fun QuackMenuFloatingActionButton(
+        @TestParameter expanded: Boolean,
         @TestParameter("0.5", "1.0", "1.5", "2.0") fontScale: Double,
     ) {
         paparazzi.boxSnapshot(
-            name = "[fontScale:$fontScale]",
+            name = "[expanded:$expanded]-[fontScale:$fontScale]",
         ) {
             QuackFontScale = fontScale
             team.duckie.quackquack.ui.component.QuackMenuFloatingActionButton(
+                expanded = expanded,
+                onClickButton = {},
+                onDismissRequest = {},
                 items = persistentListOf(
                     QuackPopUpMenuItem(
-                        quackIcon = QuackIcon.WriteFeed,
+                        icon = QuackIcon.WriteFeed,
                         text = "피드",
                         onClick = {},
                     ),
                     QuackPopUpMenuItem(
-                        quackIcon = QuackIcon.DrawerBuy,
+                        icon = QuackIcon.DrawerBuy,
                         text = "덕딜",
                         onClick = {},
                     ),
