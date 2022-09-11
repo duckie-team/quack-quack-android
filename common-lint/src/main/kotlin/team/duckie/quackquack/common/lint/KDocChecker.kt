ebcced1d5eac02618854b85fd79f1311350636ee
ricky_0_k <ridickle7@gmail.com>
2022-09-11 15:34:33 +0900

    KDocFields 린트 규칙 구현 (2차) (#91)
    * feat: KDocFieldsIssue 생성
    
    * feat: KDocFields 에 대한 TestCase 생성
    
    * feat: 일부 함수 case 추가 (즉시 반환형 함수, composable 함수, 추상 함수)
    
    * feat: ktlintFormat
    
    * bug: detekt 오류나는 내용 수정
    
    * bug: KTProperty 검사하지 않도록 수정 & 코드 수정 및 라인 정리
    
    * feat: 성빈님 리뷰 반영 및 전반적인 정책 수정
    
    1. 모호한 영어 brief 한글화
    2. it 사용한 코드 제거
    3. new line 적용
    4. 빈 내용일 경우에도 에러 발생
    5. 필수 어노테이션이 거의 없어짐에 따라 특정 어노테이션에 대해서만 린트 체크하도록 로직 대폭 수정
       - @param - 존재하는 인자들에 대해 무조건 필수
       - @return - return 이 있는 경우 필수
       - @throw - throw 가 있는 경우 필수
    
    * feat: detekt TopLevelPropertyNaming 규칙에 한글 추가
    
    * bug: 일부 detekt 오류 수정 (LongMethod)
    
    * bug: detekt 오류 수정 (_ 추가 및 앞글자 대문자 처리)
    
    * 성빈님 리뷰 1차 반영
    
    1. 한글변수 관련 내용 보강
    2. 띄어쓰기 처리
    3. ~~ 영역 dokka 에서 에러나지 않도록 작성
    4. it 제거
    5. isNoParams 로 파라미터명 변경
    6. 의미없는 안전 체크 코드 제거 및 개행 정리
    7. 테스트 함수 내 must 단어 제거
    
    * 재정의 함수는 kDoc 작성하지 않도록 함
    
    * feat 재정의 함수에 대해 새로운 정책 적용 (override 함수는 kDoc 이 있을 경우 규칙을 지키되, 없을 경우 그냥 넘어감)
    
    * feat 성빈님 리뷰 반영
    
    1. KDocTag.getContent() 확장함수화
    2. 괄호 방식 통일 (무조건 달기)
    3. 도메인적 의미 강조 문법 사용 (filterIsInstance, find)
    
    * bug: 오로지 함수만 체크하지 않던 내용 수정

---
+++ common-lint/src/main/kotlin/team/duckie/quackquack/common/lint/KDocChecker.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * [KDocChecker.kt] created by ricky_0_k on 22. 9, 11. 오후 3:21
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.common.lint
+
+import org.jetbrains.kotlin.kdoc.psi.impl.KDocTag
+
+val KDocTag.content get() = this.getContent()
