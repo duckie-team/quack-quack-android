c350fde3abba889449fb6ee6c9d0a0e46368dd63
goddoro <dongsang1222@daum.net>
2022-09-13 15:01:56 +0900

    feat: SelectableImage Kdoc 추가 및 세세하게 파라미터 변경

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/selectableImage.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/selectableImage.kt
@@ -9,41 +9,80 @@
 
 package team.duckie.quackquack.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import team.duckie.quackquack.ui.border.QuackBorder
 import team.duckie.quackquack.ui.color.QuackColor
+import team.duckie.quackquack.ui.color.QuackColor.Companion.Black80
 import team.duckie.quackquack.ui.icon.QuackIcon
+import team.duckie.quackquack.ui.modifier.quackBorderOrNull
 
-private val SelectableImageSize = 118.dp
 private val ImagePadding = PaddingValues(top = 4.dp, end = 4.dp)
-
-private val Black80 = Color(0x80222222)
+private val DeletableImageSmallSize = 46.dp
+private val DeletableImageLargeSize = 72.dp
+private val QuackDeletableImageShape = RoundedCornerShape(12.dp)
+private val QuackLargeDeletablePadding = PaddingValues(top = 6.dp, end = 6.dp)
+private val QuackSmallDeletablePadding = PaddingValues(top = 2.dp, end = 2.dp)
 
 /**
- * TODO Border 챙기기 , Icon 챙기기, 왜 영역보다 더 Clickable이 많이 되는거 같지?
+ * TODO 왜 영역보다 더 Clickable이 많이 되는거 같지?
+ *
+ * QuackSelectableImage 를 구현합니다.
+ *
+ * @param size 이미지의 길이는 화면에 따라 가변적이므로 넘겨받습니다
+ * @param isSelected 해당 이미지가 선택되었는지에 대한 상태값
+ * @param image 이미지 resource
+ * @param onClick 이미지가 선택되었을 때 발생하는 클릭 이벤트 -> 이미지 자체가 아니라 아이콘의 클릭 이벤트
+ *
+ * 클릭될 때마다 SelectedFilterBox 가 나타나게 됨
+ *
  */
 @Composable
 fun QuackSelectableImage(
+    size: Dp,
     isSelected: Boolean,
-    image: String,
-    onClick: () -> Unit
+    image: Any,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .size(size)
+            .quackBorderOrNull(
+                color = QuackColor.DuckieOrange,
+                isSelected = isSelected
+            )
+    ) {
+        QuackImage(image = image)
+        if (isSelected) SelectedFilterBox()
+        QuackSelectedIcon(
+            modifier = Modifier.align(Alignment.TopEnd),
+            isSelected = isSelected,
+            onClick = onClick,
+        )
+    }
+}
+
+/**
+ * QuackSelectedIcon 은 QuackSelectableImage 에서 사용되는 우상단 아이콘입니다.
+ *
+ * @param modifier QuackSelectedIcon 의 alignment 설정을 위해 넘겨받습니다.
+ * @param isSelected 아이콘이 선택되었는지에 대한 상태값, 어떤 아이콘을 보여줄지 결정합니다
+ * @param onClick 아이콘 클릭시 발생하는 클릭 이벤트
+ */
+@Composable
+private fun QuackSelectedIcon(
+    modifier: Modifier,
+    isSelected: Boolean,
+    onClick: () -> Unit,
 ) {
     val selectableIcon = when (isSelected) {
         true -> QuackIcon.Checked
@@ -51,151 +90,110 @@ fun QuackSelectableImage(
     }
 
     Box(
-        modifier = Modifier
-            .size(SelectableImageSize)
-            .quackBorderOrNull(
-                color = QuackColor.DuckieOrange,
-                isSelected = isSelected
-            )
-    ) {
-        Image(
-            painterResource(team.duckie.quackquack.ui.R.drawable.sample_image),
-            contentDescription = null
-        )
-        SelectedFilterBox(isSelected)
-        QuackSelectedIcon(
-            modifier = Modifier.align(Alignment.TopEnd),
-            onClick = onClick,
-            selectableIcon = selectableIcon
-        )
-        //QuackImage(image = image)
-
-    }
-}
-
-@Composable
-private fun QuackSelectedIcon(modifier: Modifier, onClick: () -> Unit, selectableIcon: QuackIcon) {
-    Box(
         modifier = modifier
             .padding(ImagePadding),
     ) {
         QuackImage(
             icon = selectableIcon,
-            onClick = onClick
+            rippleEnabled = false,
+            onClick = onClick,
         )
     }
 }
 
+/**
+ * 이미지가 선택되었을 때 나타나는 Deem Filter
+ */
 @Composable
-private fun SelectedFilterBox(isSelected: Boolean) {
-    if (!isSelected) return
+private fun SelectedFilterBox() {
     Box(
         modifier = Modifier
-            .background(Black80)
             .fillMaxSize()
+            .background(Black80.value)
     )
 }
 
-private val DeletableImageSmallSize = 46.dp
-private val DeletableImageLargeSize = 72.dp
-private val QuackDeletableImageShape = RoundedCornerShape(12.dp)
-private val Gray5 = Color(0xFFD1D1D1)
-
 /**
+ * QuackDeletableImage 의 Large Component 를 구현합니다
+ *
+ * @param image 이미지 Resource
+ * @param onClick deleteIcon 을 클릭헀을 때 발생하는 클릭 이벤트
  *
  */
-
 @Composable
 fun QuackLargeDeletableImage(
+    image: Any,
     onClick: () -> Unit,
 ) {
     Box(modifier = Modifier.size(DeletableImageLargeSize)) {
-        QuackDeleteImage(modifier = Modifier.matchParentSize())
+        QuackDeleteImage(
+            modifier = Modifier.matchParentSize(),
+            image = image,
+        )
         QuackDeleteIcon(
             modifier = Modifier
                 .align(Alignment.TopEnd)
-                .padding(top = 6.dp, end = 6.dp),
-            onClick = onClick
+                .padding(QuackLargeDeletablePadding),
+            onClick = onClick,
         )
     }
 }
 
+/**
+ * QuackDeletableImage 의 Small Component 를 구현합니다
+ * @param image 이미지 Resource
+ * @param onClick deleteIcon 을 클릭헀을 때 발생하는 클릭 이벤트
+ */
 @Composable
 fun QuackSmallDeletableImage(
+    image: Any,
     onClick: () -> Unit,
 ) {
     Box(modifier = Modifier.size(DeletableImageSmallSize)) {
         QuackDeleteImage(
-            modifier = Modifier
-                .matchParentSize()
-                .padding(top = 2.dp, end = 2.dp)
+            modifier = Modifier.matchParentSize(),
+            image = image,
         )
         QuackDeleteIcon(
-            modifier = Modifier
-                .align(Alignment.TopEnd),
-            onClick = onClick
+            modifier = Modifier.align(Alignment.TopEnd),
+            onClick = onClick,
         )
     }
 }
 
+/**
+ * Quack*DeletableImage 에서 공통적으로 사용되는 Background 이미지
+ * @param modifier matchParentSize() 값을 전달받기 위한 Modifier
+ */
 @Composable
-private fun QuackDeleteImage(modifier: Modifier) {
+private fun QuackDeleteImage(modifier: Modifier, image: Any) {
     Box(
         modifier = modifier
+            .padding(QuackSmallDeletablePadding)
             .clip(QuackDeletableImageShape)
-            .background(Gray5)
-    )
-
+    ) {
+        QuackImage(image = image)
+    }
 }
 
+/**
+ * Quack*DeletableImage 에서 공통적으로 사용되는 Delete 아이콘
+ *
+ * @param modifier topEnd 로 align 하기 위한 Modifier
+ * @param onClick 아이콘을 클릭했을 떄 발생한느 클릭 이벤트
+ */
+
 @Composable
-private fun QuackDeleteIcon(modifier: Modifier, onClick: () -> Unit) {
+private fun QuackDeleteIcon(
+    modifier: Modifier,
+    onClick: () -> Unit,
+) {
     Box(
         modifier = modifier
     ) {
-        QuackImage(icon = QuackIcon.Delete, onClick = onClick)
-    }
-}
-
-
-@Preview
-@Composable
-private fun QuackSelectableImagePreview() {
-    val isSelected = remember { mutableStateOf(false) }
-    val previewUrl = "https://picsum.photos/id/237/200/300"
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
-    ) {
-        QuackSelectableImage(isSelected = isSelected.value, image = previewUrl, onClick = {
-            isSelected.value = !isSelected.value
-        })
-    }
-}
-
-@Preview
-@Composable
-private fun QuackDeletableImagePreview(){
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
-    ){
-        QuackLargeDeletableImage {}
-        QuackSmallDeletableImage {}
-    }
-}
-
-@Stable
-internal fun Modifier.quackBorderOrNull(
-    isSelected: Boolean,
-    width: Dp = 1.dp,
-    color: QuackColor = QuackColor.DuckieOrange
-) = when (isSelected) {
-    true -> composed {
-        border(
-            border = QuackBorder(
-                width = width,
-                color = color
-            ).asComposeBorder()
+        QuackImage(
+            icon = QuackIcon.DeleteBg,
+            onClick = onClick,
         )
     }
-    else -> this
 }
