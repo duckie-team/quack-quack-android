766cb03c765ac6e1071e0e4cdce4ddbfa367d9a9
goddoro <dongsang1222@daum.net>
2022-09-14 00:46:31 +0900

    feat: QuackTopAppBar 문서화하고 parameter 정리

---
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/bottomNavigation.kt
@@ -0,0 +1,2 @@
+package team.duckie.quackquack.ui.component
+
--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/topAppBar.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/topAppBar.kt
@@ -20,21 +20,39 @@ import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.modifier.applyQuackSize
 import team.duckie.quackquack.ui.modifier.quackClickable
 
-/**
- * QuackTopAppBar 를 구현합니다.
- *
- */
-
 private val QuackTopAppBarPadding = PaddingValues(
     horizontal = 16.dp,
 )
+private val QuackTopAppBarHeight = 48.dp
+private val QuackTopAppBarLeadingIconSpacedBy = 12.dp
+private val QuackTopAppBarTrailingIconSpacedBy = 8.dp
 
 /**
  * TODO : centerContent 의 width 를 어떻게 처리를 해줘야할듯
+ * QuackTopAppBar 를 구현합니다
+ *
+ * @param modifier 다양한 Align 으로 사용하기 위해 Modifier 를 열어줍니다.
+ * @param leadingIcon start 위치에 존재하는 icon
+ * @param trailingIcon end 위치에 존재하는 icon
+ * @param secondTrailingIcon end 왼쪽에 존재하는 icon
+ * @param trailingText end 위치에 존재하는 text
+ * @param headline leadingIcon 오른쪽에 위치하는 headline
+ * @param centerContent 가운데 위치하는 Composable 람다 함수
+ * @param onClickLeadingIcon leadingIcon 클릭 이벤트
+ * @param onClickTrailingIcon trailingIcon 클릭 이벤트
+ * @param onClickSecondTrailingIcon secondTrailingIcon 클릭 이벤트
+ * @param onClickTrailingText trailingText 클릭 이벤트
+ *
+ * 여러 가지 형태의 TopAppBar 가 존재하지만 하나의 함수로 구현하였고,
+ * 필요한 데이터만 파라미터로 넘겨받아 Composable 함수 호출가능
+ *
+ * 또한 데이터만 넣고 onClick 이벤트를 넣지 않을 경우에는 [checkTopAppBarValidation] 에서 Assert 로 체크
+ *
  */
 
 @Composable
 fun QuackTopAppBar(
+    modifier: Modifier = Modifier,
     leadingIcon: QuackIcon,
     trailingIcon: QuackIcon? = null,
     secondTrailingIcon: QuackIcon? = null,
@@ -55,17 +73,17 @@ fun QuackTopAppBar(
         onClickTrailingText = onClickTrailingText,
     )
     Box(
-        modifier = Modifier
+        modifier = modifier
             .applyQuackSize(
                 width = QuackWidth.Fill,
-                height = QuackHeight.Custom(48.dp),
+                height = QuackHeight.Custom(QuackTopAppBarHeight),
             )
-            .background(QuackColor.White.value)
-            .padding(QuackTopAppBarPadding)
+            .background(color = QuackColor.White.value)
+            .padding(paddingValues = QuackTopAppBarPadding)
     ) {
         Row(
             modifier = Modifier.align(Alignment.CenterStart),
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(QuackTopAppBarLeadingIconSpacedBy),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             QuackImage(
@@ -81,7 +99,7 @@ fun QuackTopAppBar(
 
         Row(
             modifier = Modifier.align(Alignment.CenterEnd),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(QuackTopAppBarTrailingIconSpacedBy),
         ) {
             if (secondTrailingIcon != null) {
                 QuackImage(
@@ -106,8 +124,8 @@ fun QuackTopAppBar(
         }
 
         Box(
-            modifier = Modifier.align(Alignment.Center),
-        ){
+            modifier = Modifier.align(alignment = Alignment.Center),
+        ) {
             centerContent?.invoke()
         }
     }
@@ -120,15 +138,15 @@ private fun checkTopAppBarValidation(
     onClickTrailingIcon: (() -> Unit)? = null,
     onClickSecondTrailingIcon: (() -> Unit)? = null,
     onClickTrailingText: (() -> Unit)? = null,
-){
+) {
     assert(trailingText == null || trailingIcon == null)
-    if ( secondTrailingIcon != null){
+    if (secondTrailingIcon != null) {
         assert(trailingIcon != null && onClickSecondTrailingIcon != null)
     }
-    if ( trailingIcon != null){
+    if (trailingIcon != null) {
         assert(onClickTrailingIcon != null)
     }
-    if ( trailingText != null){
+    if (trailingText != null) {
         assert(onClickTrailingText != null)
     }
 }
