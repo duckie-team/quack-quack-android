b8787a0eae79ae22c0c40dcf3929b87b314bbaa5
jisungbin <ji@sungb.in>
2022-09-15 16:28:04 +0900

    chore: 그레이들 evaluate 시점 변경

--- build-logic/src/main/kotlin/AndroidQuackPublishPlugin.kt
+++ build-logic/src/main/kotlin/AndroidQuackPublishPlugin.kt
@@ -7,6 +7,7 @@
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.dependencies
 import team.duckie.quackquack.convention.Empty
@@ -26,32 +27,36 @@ class AndroidQuackPublishPlugin : Plugin<Project> {
                 String.Empty, QuackArtifactType.Empty,
             )
 
-            if (extension.version.isBlank() || extension.type.isContainsBlank()) return
+            afterEvaluate {
+                if (extension.version.isBlank() || extension.type.isContainsBlank()) return@afterEvaluate
 
-            ext {
-                val properties = mapOf(
-                    "PUBLISH_VERSION" to extension.version,
-                    "PUBLISH_GROUP_ID" to "team.duckie.quack",
-                    "PUBLISH_ARTIFACT_ID" to "quack-lint-${extension.type.artifactName}",
-                    "PUBLISH_DESCRIPTION" to extension.type.description,
-                    "PUBLISH_URL" to "https://github.com/sungbinland/duckie-quack-quack",
-                    "PUBLISH_LICENSE_NAME" to "MIT License",
-                    "PUBLISH_LICENSE_URL" to "https://github.com/sungbinland/duckie-quack-quack/blob/develop/LICENSE",
-                    "PUBLISH_DEVELOPER_ID" to "jisungbin",
-                    "PUBLISH_DEVELOPER_NAME" to "Ji Sungbin",
-                    "PUBLISH_DEVELOPER_EMAIL" to "ji@sungb.in",
-                    "PUBLISH_SCM_CONNECTION" to "scm:git:github.com/sungbinland/duckie-quack-quack.git",
-                    "PUBLISH_SCM_DEVELOPER_CONNECTION" to "scm:git:ssh://github.com/sungbinland/duckie-quack-quack.git",
-                    "PUBLISH_SCM_URL" to "https://github.com/sungbinland/duckie-quack-quack/tree/main",
-                )
+                ext {
+                    val properties = mapOf(
+                        "PUBLISH_VERSION" to extension.version,
+                        "PUBLISH_GROUP_ID" to "team.duckie.quack",
+                        "PUBLISH_ARTIFACT_ID" to "quack-lint-${extension.type.artifactName}",
+                        "PUBLISH_DESCRIPTION" to extension.type.description,
+                        "PUBLISH_URL" to "https://github.com/sungbinland/duckie-quack-quack",
+                        "PUBLISH_LICENSE_NAME" to "MIT License",
+                        "PUBLISH_LICENSE_URL" to "https://github.com/sungbinland/duckie-quack-quack/blob/develop/LICENSE",
+                        "PUBLISH_DEVELOPER_ID" to "jisungbin",
+                        "PUBLISH_DEVELOPER_NAME" to "Ji Sungbin",
+                        "PUBLISH_DEVELOPER_EMAIL" to "ji@sungb.in",
+                        "PUBLISH_SCM_CONNECTION" to "scm:git:github.com/sungbinland/duckie-quack-quack.git",
+                        "PUBLISH_SCM_DEVELOPER_CONNECTION" to "scm:git:ssh://github.com/sungbinland/duckie-quack-quack.git",
+                        "PUBLISH_SCM_URL" to "https://github.com/sungbinland/duckie-quack-quack/tree/main",
+                    )
 
-                properties.forEach { (key, value) ->
-                    set(key, value)
+                    properties.forEach { (key, value) ->
+                        set(key, value)
+                    }
                 }
-            }
 
-            dependencies {
-                lintPublish(extension.type.deployModuleArtifactName)
+                apply(from = "${rootDir}/scripts/publish-module.gradle")
+
+                dependencies {
+                    lintPublish(extension.type.deployModuleArtifactName)
+                }
             }
         }
     }
