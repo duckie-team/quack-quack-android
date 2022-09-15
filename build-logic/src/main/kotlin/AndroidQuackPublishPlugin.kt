2c9fd5a3d90cee45303a52c3e43258453b2ed6f2
jisungbin <ji@sungb.in>
2022-09-15 16:10:53 +0900

    chore: Quack 아티펙트 배포 플러그인 추가

---
+++ build-logic/src/main/kotlin/AndroidQuackPublishPlugin.kt
@@ -0,0 +1,58 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.dependencies
+import team.duckie.quackquack.convention.Empty
+import team.duckie.quackquack.convention.QuackArtifactType
+import team.duckie.quackquack.convention.QuackPublishExtension
+import team.duckie.quackquack.convention.ext
+import team.duckie.quackquack.convention.lintPublish
+
+private const val QuackLintPublishExtensionName = "quackLintPublish"
+
+class AndroidQuackPublishPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            val extension = project.extensions.create<QuackPublishExtension>(
+                name = QuackLintPublishExtensionName,
+                /*default constructor = */
+                String.Empty, QuackArtifactType.Empty,
+            )
+
+            if (extension.version.isBlank() || extension.type.isContainsBlank()) return
+
+            ext {
+                val properties = mapOf(
+                    "PUBLISH_VERSION" to extension.version,
+                    "PUBLISH_GROUP_ID" to "team.duckie.quack",
+                    "PUBLISH_ARTIFACT_ID" to "quack-lint-${extension.type.artifactName}",
+                    "PUBLISH_DESCRIPTION" to extension.type.description,
+                    "PUBLISH_URL" to "https://github.com/sungbinland/duckie-quack-quack",
+                    "PUBLISH_LICENSE_NAME" to "MIT License",
+                    "PUBLISH_LICENSE_URL" to "https://github.com/sungbinland/duckie-quack-quack/blob/develop/LICENSE",
+                    "PUBLISH_DEVELOPER_ID" to "jisungbin",
+                    "PUBLISH_DEVELOPER_NAME" to "Ji Sungbin",
+                    "PUBLISH_DEVELOPER_EMAIL" to "ji@sungb.in",
+                    "PUBLISH_SCM_CONNECTION" to "scm:git:github.com/sungbinland/duckie-quack-quack.git",
+                    "PUBLISH_SCM_DEVELOPER_CONNECTION" to "scm:git:ssh://github.com/sungbinland/duckie-quack-quack.git",
+                    "PUBLISH_SCM_URL" to "https://github.com/sungbinland/duckie-quack-quack/tree/main",
+                )
+
+                properties.forEach { (key, value) ->
+                    set(key, value)
+                }
+            }
+
+            dependencies {
+                lintPublish(extension.type.deployModuleArtifactName)
+            }
+        }
+    }
+}
---
+++ build-logic/src/main/kotlin/team/duckie/quackquack/convention/QuackPublishExtension.kt
@@ -0,0 +1,71 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.convention
+
+// data class 하면 final 이라 안됨
+open class QuackPublishExtension(
+    open var version: String,
+    open var type: QuackArtifactType,
+)
+
+sealed class QuackArtifactType(
+    val artifactName: String,
+    val description: String,
+    val deployModuleArtifactName: String,
+) {
+    // 플러그인 초기화에 사용하는 기본 값으로 쓰임
+    internal object Empty : QuackArtifactType(
+        artifactName = String.Empty,
+        description = String.Empty,
+        deployModuleArtifactName = String.Empty,
+    )
+
+    object UiComponents : QuackArtifactType(
+        artifactName = "ui-components",
+        description = "Duckie's design system core module",
+        deployModuleArtifactName = ":ui-components",
+    )
+
+    object LintCompose : QuackArtifactType(
+        artifactName = "compose",
+        description = buildLintArtifactDescription(
+            target = "Jetpack Compose codebase",
+        ),
+        deployModuleArtifactName = ":lint-compose",
+    )
+
+    object LintCore : QuackArtifactType(
+        artifactName = "core",
+        description = buildLintArtifactDescription(
+            target = "Duckie codebase",
+        ),
+        deployModuleArtifactName = ":lint-core",
+    )
+
+    object LintQuack : QuackArtifactType(
+        artifactName = "quack",
+        description = buildLintArtifactDescription(
+            target = "QuackQuack ui components",
+        ),
+        deployModuleArtifactName = ":lint-quack",
+    )
+
+    /*object LintWriting : QuackArtifactType(
+        artifactName = "writing",
+        description = buildLintArtifactDescription(
+            target = "UX Writing",
+        ),
+        modulePackage= "lint.writing.publish",
+    )*/
+
+    internal fun isContainsBlank() = artifactName.isBlank() || description.isBlank()
+}
+
+private fun buildLintArtifactDescription(
+    target: String,
+) = "Duckie's design system custom lint for $target"
