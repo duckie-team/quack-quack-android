f189777f111cf531e1ee3e2ae8ab311e4dfc8923
jisungbin <ji@sungb.in>
2022-09-26 06:14:51 +0900

    feat: 플레이그라운드 업데이트

--- playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
@@ -9,14 +9,18 @@ package team.duckie.quackquack.playground
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.first
 import team.duckie.quackquack.playground.base.BaseActivity
@@ -48,6 +52,13 @@ class MainActivity : BaseActivity() {
             /*SingleDemo {
                 QuackBasicTextFieldWithAllDecorationDemo()
             }*/
+            /*MultiDemo {
+                QuackTextFieldWithNoDecorationDemo()
+                QuackTextFieldWithLeadingDecorationDemo()
+                QuackTextFieldWithTrailingDecorationDemo()
+                QuackTextFieldWithAllDecorationDemo()
+                QuackTextFieldErrorStateDemo()
+            }*/
         }
     }
 }
@@ -58,7 +69,7 @@ class MainActivity : BaseActivity() {
  * @param content 표시할 단일 디자인 컴포넌트
  */
 @Composable
-fun SingleDemo(
+private fun SingleDemo(
     content: @Composable BoxScope.() -> Unit,
 ) {
     Box(
@@ -69,23 +80,44 @@ fun SingleDemo(
 }
 
 /**
+ * 여러 디자인 컴포넌트를 보여주는 데모 컴포저블 입니다.
+ *
+ * @param content 표시할 디자인 컴포넌트들
+ */
+@Composable
+private fun MultiDemo(
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.CenterVertically,
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = content,
+    )
+}
+
+/**
  * 모든 디자인 컴포넌트를 리스트 형식으로 보여주는 데모 컴포저블 입니다.
  * [PlaygroundActivities] 에 포함된 액티비티에 있는 모든 디자인 컴포넌트를 보여줍니다.
  */
 @Composable
-fun PlaygroundDemo() {
+private fun PlaygroundDemo() {
     val context = LocalContext.current.applicationContext
     LaunchedEffect(
         key1 = Unit,
     ) {
         context.dataStore.data.first().let { preference ->
-            QuackAnimationMillis = (
-                    preference[PreferenceConfigs.AnimationDurationKey]
-                        ?: QuackAnimationMillis
-                    ).coerceAtLeast(minimumValue = 0)
-            QuackFontScale = (
-                    preference[PreferenceConfigs.FontScaleKey] ?: QuackFontScale
-                    ).coerceAtLeast(minimumValue = 0.0)
+            QuackAnimationMillis = (preference[PreferenceConfigs.AnimationDurationKey]
+                ?: QuackAnimationMillis).coerceAtLeast(
+                minimumValue = 0,
+            )
+            QuackFontScale = (preference[PreferenceConfigs.FontScaleKey]
+                ?: QuackFontScale).coerceAtLeast(
+                minimumValue = 0.0,
+            )
         }
     }
     PlaygroundTheme {
