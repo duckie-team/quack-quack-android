f8a58c96b707bfc83779ce724551be3ff17abd74
jisungbin <ji@sungb.in>
2022-09-26 05:34:59 +0900

    feat: Modifier.noRippleClickable 유틸 추가

---
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/util/clickable.kt
@@ -0,0 +1,24 @@
+/*
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/sungbinland/quack-quack/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.playground.util
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+internal fun Modifier.noRippleClickable(
+    onClick: () -> Unit,
+) = composed {
+    clickable(
+        onClick = onClick,
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+    )
+}
