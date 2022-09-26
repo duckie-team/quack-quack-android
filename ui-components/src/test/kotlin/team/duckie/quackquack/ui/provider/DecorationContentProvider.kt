e9d4cfddc02a5e4d4fd962aaafb79bfcc19cbacb
jisungbin <ji@sungb.in>
2022-09-26 09:23:00 +0900

    refactor: DecorationContentProvider 에서 name 조정

--- ui-components/src/test/kotlin/team/duckie/quackquack/ui/provider/DecorationContentProvider.kt
+++ ui-components/src/test/kotlin/team/duckie/quackquack/ui/provider/DecorationContentProvider.kt
@@ -7,9 +7,9 @@
 
 package team.duckie.quackquack.ui.provider
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.google.testing.junit.testparameterinjector.TestParameter
+import team.duckie.quackquack.ui.component.QuackBody1
 import team.duckie.quackquack.ui.component.QuackImage
 import team.duckie.quackquack.ui.icon.QuackIcon
 import team.duckie.quackquack.ui.wrapper.NamedValue
@@ -18,7 +18,7 @@ class DecorationContentProvider : TestParameter.TestParameterValuesProvider {
     override fun provideValues() = listOf<NamedValue<(@Composable () -> Unit)?>>(
         NamedValue(
             value = null,
-            name = "NoDecorationContent",
+            name = "none",
         ),
         NamedValue(
             value = {
@@ -26,15 +26,15 @@ class DecorationContentProvider : TestParameter.TestParameterValuesProvider {
                     icon = QuackIcon.FilledHeart,
                 )
             },
-            name = "IconDecorationContent",
+            name = "icon",
         ),
         NamedValue(
             value = {
-                Text(
+                QuackBody1(
                     text = "LongDecorationContent",
                 )
             },
-            name = "LongDecorationContent",
+            name = "long-text",
         ),
     )
 }
