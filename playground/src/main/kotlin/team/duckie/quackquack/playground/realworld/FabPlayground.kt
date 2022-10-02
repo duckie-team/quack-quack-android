88118abeb3c5844f0a082f534064b0892c940667
jisungbin <ji@sungb.in>
2022-10-02 18:13:01 +0900

    feat: 플레이그라운드 최신화

--- playground/src/main/kotlin/team/duckie/quackquack/playground/realworld/FabPlayground.kt
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/realworld/FabPlayground.kt
@@ -9,11 +9,16 @@ package team.duckie.quackquack.playground.realworld
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.persistentListOf
 import team.duckie.quackquack.playground.base.BaseActivity
 import team.duckie.quackquack.playground.base.PlaygroundSection
@@ -54,33 +59,40 @@ fun QuackFabDemo() {
 
 @Composable
 fun QuackMenuFabDemo() {
-    val toast = rememberToast()
-    var state by remember {
-        mutableStateOf(
-            value = false,
+    Box(
+        modifier = Modifier.size(
+            size = 250.dp,
+        ),
+        contentAlignment = Alignment.BottomEnd,
+    ) {
+        val toast = rememberToast()
+        var state by remember {
+            mutableStateOf(
+                value = false,
+            )
+        }
+        QuackMenuFab(
+            items = persistentListOf(
+                QuackMenuFabItem(
+                    icon = QuackIcon.FilledHeart,
+                    text = "FilledHeart",
+                ),
+                QuackMenuFabItem(
+                    icon = QuackIcon.Heart,
+                    text = "Heart",
+                ),
+                QuackMenuFabItem(
+                    icon = QuackIcon.WhiteHeart,
+                    text = "WhiteHeart",
+                ),
+            ),
+            expanded = state,
+            onFabClick = { state = !state },
+            onItemClick = { _, item ->
+                toast(
+                    message = item,
+                )
+            },
         )
     }
-    QuackMenuFab(
-        items = persistentListOf(
-            QuackMenuFabItem(
-                icon = QuackIcon.FilledHeart,
-                text = "FilledHeart",
-            ),
-            QuackMenuFabItem(
-                icon = QuackIcon.Heart,
-                text = "Heart",
-            ),
-            QuackMenuFabItem(
-                icon = QuackIcon.WhiteHeart,
-                text = "WhiteHeart",
-            ),
-        ),
-        expanded = state,
-        onFabClick = { state = !state },
-        onItemClick = { _, item ->
-            toast(
-                message = item,
-            )
-        },
-    )
 }
