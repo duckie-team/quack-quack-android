7b9917803f1e694642969196e0ce023be0cf9830
goddoro <dongsang1222@daum.net>
2022-10-06 05:08:11 +0900

    feat: Transparent 색깔 추가

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/color/QuackColor.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/color/QuackColor.kt
@@ -155,6 +155,13 @@ value class QuackColor internal constructor(
             ),
         )
 
+        @Stable
+        val Transparent = QuackColor(
+            value = Color(
+                color = 0x00000000,
+            )
+        )
+
         private val M1 = floatArrayOf(
             0.80405736f,
             0.026893456f,
