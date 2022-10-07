1b30379c99bc51927c4a5c7ab59d0c38e91c2798
jisungbin <ji@sungb.in>
2022-10-08 05:08:39 +0900

    test: TODO 테스트에 ignore 추가

--- ui-components-snapshots/src/test/kotlin/team/duckie/quackquack/ui/snapshot/QuackFab.kt
+++ ui-components-snapshots/src/test/kotlin/team/duckie/quackquack/ui/snapshot/QuackFab.kt
@@ -12,23 +12,23 @@
 
 package team.duckie.quackquack.ui.snapshot
 
-import app.cash.paparazzi.Paparazzi
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Ignore
 import org.junit.Rule
-import org.junit.rules.TestWatcher
 import org.junit.runner.RunWith
 import team.duckie.quackquack.ui.snapshot.rule.AnimationTestRule
 import team.duckie.quackquack.ui.snapshot.util.buildPaparazzi
 
+@Ignore(
+    value = "TODO",
+)
 @RunWith(TestParameterInjector::class)
 class QuackFab {
     @get:Rule
-    val paparazzi: Paparazzi = buildPaparazzi {
+    val paparazzi = buildPaparazzi {
         screenHeight = 500
     }
 
     @get:Rule
-    val animationTest: TestWatcher = AnimationTestRule()
-
-    // TODO
+    val animationTest = AnimationTestRule()
 }
