7b664c70cf18d0a881743f9a125a6571f93253f6
jisungbin <ji@sungb.in>
2022-10-08 07:57:28 +0900

    feat: QuackIcon 은 Modifier#paint 로 그리게 변경

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/image.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/image.kt
@@ -7,13 +7,17 @@
 
 package team.duckie.quackquack.ui.component
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.DpSize
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -28,7 +32,6 @@ import team.duckie.quackquack.ui.util.runIf
  * 이미지 혹은 [QuackIcon] 을 표시합니다.
  *
  * @param src 표시할 리소스. 만약 null 이 들어온다면 리소스를 그리지 않습니다.
- * [GlideImage] 에서 지원하는 [Any] 에 추가로 [QuackIcon] 이 포함될 수 있습니다.
  * @param overrideSize 리소스의 크기를 지정합니다. null 이 들어오면 기본 크기로 표시합니다.
  * @param tint 아이콘에 적용할 틴트 값
  * @param rippleEnabled 이미지 클릭시 ripple 발생 여부
@@ -70,26 +73,43 @@ internal fun QuackImageInternal(
     tint: QuackColor? = null,
 ) {
     if (src == null) return
-    val imageModel = remember(
-        key1 = src,
-    ) {
-        if (src is QuackIcon) src.drawableId else src
-    }
-    val animatedTint = tint?.let {
-        animateQuackColorAsState(
-            targetValue = tint,
-        )
-    }
     val density = LocalDensity.current
+    val animatedTint by animateQuackColorAsState(
+        targetValue = tint ?: QuackColor.Transparent,
+    )
+    if (src is QuackIcon) {
+        AnimatedContentTransform(
+            targetState = src,
+        ) { imageModel ->
+            Box(
+                modifier = modifier
+                    .runIf(
+                        condition = overrideSize != null,
+                    ) {
+                        size(
+                            size = overrideSize!! * density.fontScale,
+                        )
+                    }
+                    .paint(
+                        painter = painterResource(
+                            id = imageModel.drawableId,
+                        ),
+                        colorFilter = animatedTint.toColorFilter(),
+                        contentScale = ContentScale.Fit,
+                    )
+            )
+        }
+        return
+    }
     AnimatedContentTransform(
-        targetState = imageModel,
-    ) { animatedImageModel ->
+        targetState = src,
+    ) { imageModel ->
         AsyncImage(
             modifier = modifier.runIf(
                 condition = overrideSize != null,
             ) {
                 size(
-                    size = overrideSize!!,
+                    size = overrideSize!! * density.fontScale,
                 )
             },
             model = ImageRequest
@@ -97,7 +117,7 @@ internal fun QuackImageInternal(
                     context = LocalContext.current,
                 )
                 .data(
-                    data = animatedImageModel,
+                    data = imageModel,
                 )
                 .runIf(
                     condition = overrideSize != null,
@@ -105,15 +125,15 @@ internal fun QuackImageInternal(
                     with(
                         receiver = density,
                     ) {
-                        val size = overrideSize!!
+                        val size = overrideSize!! * density.density
                         size(
-                            width = (size.width * density.density).roundToPx(),
-                            height = (size.height * density.density).roundToPx(),
+                            width = size.width.roundToPx(),
+                            height = size.height.roundToPx(),
                         )
                     }
                 }
                 .build(),
-            colorFilter = animatedTint?.value?.toColorFilter(),
+            colorFilter = animatedTint.toColorFilter(),
             contentDescription = null,
         )
     }
@@ -123,10 +143,11 @@ internal fun QuackImageInternal(
  * [QuackColor] 를 [ColorFilter] 로 변환합니다.
  *
  * @receiver 변환할 [QuackColor]
- * @return 변환된 [ColorFilter], 만약 receiver 로 null 이 들어왔다면 null 을 그대로 반환합니다.
+ * @return 변환된 [ColorFilter]. 만약 [QuackColor] 가 [QuackColor.Transparent] 이라면
+ * null 을 반환합니다.
  */
-private fun QuackColor?.toColorFilter() = this?.run {
-    ColorFilter.tint(
+private fun QuackColor.toColorFilter() =
+    if (this == QuackColor.Transparent) null
+    else ColorFilter.tint(
         color = composeColor,
     )
-}
