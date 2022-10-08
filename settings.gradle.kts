d432e51a47fba44b378156ca267d48c69dd2dde8
jisungbin <ji@sungb.in>
2022-10-08 10:32:49 +0900

    refactor: 저작권 주석 재정렬

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 /*
-* Designed and developed by 2022 SungbinLand, Team Duckie
-*
-* Licensed under the MIT.
-* Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
-*/
+ * Designed and developed by 2022 SungbinLand, Team Duckie
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ */
 
 @file:Suppress("UnstableApiUsage")
 
