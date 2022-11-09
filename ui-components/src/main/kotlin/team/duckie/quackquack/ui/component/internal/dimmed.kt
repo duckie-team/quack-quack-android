dec3a81ab5c3118344d5610778a5dd2ade6b346f
jisungbin <ji@sungb.in>
2022-11-09 21:07:22 +0900

    refactor: dimmed 리팩토링
    - 빠진 문서 보충
    - color 옵션 제거

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/internal/dimmed.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/internal/dimmed.kt
@@ -15,6 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.zIndex
 
+/**
+ * [QuackBackgroundDimmed] 의 기본 색상
+ */
 private val QuackDefaultBackgroundDimmedColor = Color.Black.copy(
     alpha = 0.6f,
 )
@@ -26,13 +29,11 @@ private val QuackDefaultBackgroundDimmedColor = Color.Black.copy(
  * 전체 화면을 주어진 dimmed color 로 꽉 채우는 방식이기 때문에 이 컴포저블을 사용하는
  * 루트 컴포저블의 레이아웃 범위가 `match_parent` 이여야 합니다.
  *
- * @param color dimmed 색상
  * @param zIndex dimmed 레이어의 z-index
  * @param enabled dimmed 를 활성화할지 여부
  */
 @Composable
 internal fun QuackBackgroundDimmed(
-    color: Color = QuackDefaultBackgroundDimmedColor,
     zIndex: Float = 0f,
     enabled: Boolean,
 ) {
@@ -41,7 +42,7 @@ internal fun QuackBackgroundDimmed(
             modifier = Modifier
                 .fillMaxSize()
                 .background(
-                    color = color,
+                    color = QuackDefaultBackgroundDimmedColor,
                 )
                 .zIndex(
                     zIndex = zIndex,
