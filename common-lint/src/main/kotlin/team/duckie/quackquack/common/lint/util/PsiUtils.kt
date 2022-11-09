b9f4ad880f14c27f333284ce7b1c9bfb5adb060c
Ji Sungbin <ji@sungb.in>
2022-11-09 21:03:33 +0900

    refactor: 라이선스 주소 최신화 (#357)
    * refactor: 라이선스 주소 최신화
    
    * refactor: textfield 최신화
    
    * refactor: textfield/textarea 최신화

--- common-lint/src/main/kotlin/team/duckie/quackquack/common/lint/util/PsiUtils.kt
+++ common-lint/src/main/kotlin/team/duckie/quackquack/common/lint/util/PsiUtils.kt
@@ -2,7 +2,7 @@
  * Designed and developed by 2022 SungbinLand, Team Duckie
  *
  * Licensed under the MIT.
- * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/master/LICENSE
  */
 
 // TAKEN FROM: https://github.com/androidx/androidx/tree/androidx-main/compose/lint/common/src/main/java/androidx/compose/lint
