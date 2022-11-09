08e692feebe18f0495ddc7091883639f92170cd1
jisungbin <ji@sungb.in>
2022-11-09 20:25:42 +0900

    refactor: 라이선스 주소 최신화

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/image.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/image.kt
@@ -2,7 +2,7 @@
  * Designed and developed by 2022 SungbinLand, Team Duckie
  *
  * Licensed under the MIT.
- * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/master/LICENSE
  */
 
 package team.duckie.quackquack.ui.component
