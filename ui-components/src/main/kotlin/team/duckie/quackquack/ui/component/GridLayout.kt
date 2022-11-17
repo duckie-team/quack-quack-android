13303566075098006c23ad32304e8b7e01a44739
jisungbin <ji@sungb.in>
2022-11-18 00:17:01 +0900

    refactor: wip

--- ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/GridLayout.kt
+++ ui-components/src/main/kotlin/team/duckie/quackquack/ui/component/GridLayout.kt
@@ -7,235 +7,100 @@
 
 package team.duckie.quackquack.ui.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridItemSpanScope
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.collections.immutable.PersistentList
-import team.duckie.quackquack.ui.modifier.quackClickable
+import kotlinx.collections.immutable.ImmutableList
 
 /**
  * QuackGridLayout 를 그리는데 필요한 리소스들을 정의합니다.
  */
 private object QuackGridDefaults {
+    val HorizontalGap = 3.dp
+    val VerticalGap = 3.dp
 
+    /**
+     * 한 줄당 들어갈 수 있는 최대 컴포넌트 수
+     */
+    const val MaxComponentCountsForLine = 3
 }
 
 /**
- * [QuackSimpleGridLayout] 을 구현합니다.
+ * Duckie implements the GridLayout component used for the gallery view.
  *
- * @param columns Grid가 몇 줄로 구성되어있는지에 대한 값
- * @param items List로 사용할 아이템 데이터
- * @param verticalSpace GridLayout 의 아이템간의 수직 간격
- * @param horizontalSpace GridLayout 의 아이템간의 수평 간격
- * @param contentPadding GridLayout 의 레이아웃 자체의 내부 패딩
- * @param itemContent 구현할 아이템의 Composable 함수
- *
+ * @param modifier the modifier to apply to this layout
+ * @param items the data list
+ * @param state the state object to be used to control or observe the list's state
+ * @param key a factory of stable and unique keys representing the item. Using the same key
+ * for multiple items in the grid is not allowed. Type of the key should be saveable
+ * via Bundle on Android. If null is passed the position in the grid will represent the key.
+ * When you specify the key the scroll position will be maintained based on the key, which
+ * means if you add/remove items before the current visible item the item with the given key
+ * will be kept as the first visible one.
+ * @param span define custom spans for the items. Default is 1x1. It is good practice to leave
+ * it `null` when this matches the intended behavior, as providing a custom implementation
+ * impacts performance
+ * @param contentType a factory of the content types for the item. The item compositions of
+ * the same type could be reused more efficiently. Note that null is a valid type and items of such
+ * type will be considered compatible.
+ * @param itemContent the content displayed by a single item
  */
 @Composable
-public fun <T> QuackSimpleGridLayout(
-    columns: Int,
-    items: PersistentList<T>,
-    verticalSpace: Dp = QuackVerticalSpacePadding,
-    horizontalSpace: Dp = QuackHorizontalSpacePadding,
-    contentPadding: PaddingValues = QuackGridLayoutContentPadding,
-    itemContent: @Composable (Int, T) -> Unit,
+public fun <T> QuackGridLayout(
+    modifier: Modifier = Modifier,
+    items: ImmutableList<T>,
+    state: LazyGridState = rememberLazyGridState(),
+    key: ((
+        index: Int,
+        item: T,
+    ) -> Any)? = null,
+    span: (LazyGridItemSpanScope.(
+        index: Int,
+        item: T,
+    ) -> GridItemSpan)? = null,
+    contentType: (
+        index: Int,
+        item: T,
+    ) -> Any? = { _, _ ->
+        null
+    },
+    itemContent: @Composable (
+        index: Int, item: T,
+    ) -> Unit,
+): Unit = with(
+    receiver = QuackGridDefaults,
 ) {
     LazyVerticalGrid(
+        modifier = modifier,
+        state = state,
         columns = GridCells.Fixed(
-            count = columns
+            count = MaxComponentCountsForLine
         ),
-        contentPadding = contentPadding,
-        content = {
-            itemsIndexed(
-                items = items,
-            ) { index, item ->
-                QuackSimpleGridItem(
-                    verticalSpace = verticalSpace,
-                    horizontalSpace = horizontalSpace,
-                    itemContent = {
-                        itemContent(
-                            index,
-                            item,
-                        )
-                    },
-                    index = index,
-                    size = items.size,
-                )
-            }
-        },
-    )
-}
-
-/**
- * [QuackHeaderGridLayout] 을 구현합니다.
- *
- * GridLayout 중에 첫 아이템만 다른 아이템으로 사용해야하는 UI에 사용할 수 있습니다.
- *
- * @param columns Grid가 몇 줄로 구성되어있는지에 대한 값
- * @param items List로 사용할 아이템 데이터
- * @param verticalSpace GridLayout 의 아이템간의 수직 간격
- * @param horizontalSpace GridLayout 의 아이템간의 수평 간격
- * @param contentPadding GridLayout 의 레이아웃 자체의 내부 패딩
- * @param onClickHeader GridLayout의 헤더 아이템의 클릭 이벤트
- * @param itemContent 구현할 아이템의 Composable 함수
- * @param header 구현할 VerticalGridLayout의 첫 번째 아이템
- */
-@Composable
-public fun <T> QuackHeaderGridLayout(
-    columns: Int,
-    items: PersistentList<T>,
-    verticalSpace: Dp = QuackVerticalSpacePadding,
-    horizontalSpace: Dp = QuackHorizontalSpacePadding,
-    contentPadding: PaddingValues = QuackGridLayoutContentPadding,
-    onClickHeader: () -> Unit,
-    itemContent: @Composable (Int, T) -> Unit,
-    header: @Composable () -> Unit,
-) {
-    val gridItems = listOf(null) + items
-
-    LazyVerticalGrid(
-        contentPadding = contentPadding,
-        columns = GridCells.Fixed(
-            count = columns
+        verticalArrangement = Arrangement.spacedBy(
+            space = VerticalGap,
         ),
-        content = {
-            itemsIndexed(
-                items = gridItems,
-            ) { index, item ->
-                QuackSimpleGridItem(
-                    verticalSpace = verticalSpace,
-                    horizontalSpace = horizontalSpace,
-                    itemContent = {
-                        if (item != null) {
-                            itemContent(
-                                index,
-                                item,
-                            )
-                        }
-                    },
-                    header = header,
-                    index = index,
-                    size = gridItems.size,
-                    onClickHeader = onClickHeader,
-                )
-            }
-        },
-    )
-}
-
-/**
- * [QuackHeaderGridLayout] 을 구현합니다.
- *
- * GridLayout 중에 첫 아이템만 다른 아이템으로 사용해야하는 UI에 사용할 수 있습니다.
- *
- * @param columns Grid가 몇 줄로 구성되어있는지에 대한 값
- * @param items List로 사용할 아이템 데이터
- * @param verticalSpace GridLayout 의 아이템간의 수직 간격
- * @param horizontalSpace GridLayout 의 아이템간의 수평 간격
- * @param contentPadding GridLayout 의 레이아웃 자체의 내부 패딩
- * @param onClickFooter GridLayout의 마지막 아이템의 클릭 이벤트
- * @param itemContent 구현할 아이템의 Composable 함수
- * @param footer 구현할 VerticalGridLayout의 마지막 아이템
- */
-@Composable
-public fun <T> QuackFooterGridLayout(
-    columns: Int,
-    items: PersistentList<T>,
-    verticalSpace: Dp = QuackVerticalSpacePadding,
-    horizontalSpace: Dp = QuackHorizontalSpacePadding,
-    contentPadding: PaddingValues = QuackGridLayoutContentPadding,
-    onClickFooter: () -> Unit,
-    itemContent: @Composable (Int, T) -> Unit,
-    footer: @Composable () -> Unit,
-) {
-    val gridItems = items + listOf(null)
-
-    LazyVerticalGrid(
-        contentPadding = contentPadding,
-        columns = GridCells.Fixed(
-            count = columns
+        horizontalArrangement = Arrangement.spacedBy(
+            space = HorizontalGap,
         ),
-        content = {
-            itemsIndexed(
-                items = gridItems,
-            ) { index, item ->
-                QuackSimpleGridItem(
-                    verticalSpace = verticalSpace,
-                    horizontalSpace = horizontalSpace,
-                    itemContent = {
-                        if (item != null) {
-                            itemContent(
-                                index,
-                                item,
-                            )
-                        }
-                    },
-                    footer = footer,
-                    index = index,
-                    size = gridItems.size,
-                    onClickFooter = onClickFooter,
-                )
-            }
-        },
-    )
-}
-
-/**
- * [QuackSimpleGridItem] 를 구현합니다
- *
- * @param index 현재 아이템의 index
- * @param size 아이템의 갯수
- * @param verticalSpace 아이템간의 수직 간격
- * @param horizontalSpace 아이템간의 수평 간격
- * @param itemContent 일반적인 GridLayout 에서 사용되는 item @Composable 함수
- * @param footer 마지막 아이템으로 올 수 있는 @Composable 함수
- * @param onClickFooter 마지막 아이템의 클릭 이벤트
- * @param header 첫 번째 아이템으로 올 수 있는 @Composable 함수
- * @param onClickHeader 첫 번쨰 아이템의 클릭 이벤트
- */
-
-@Composable
-private fun QuackSimpleGridItem(
-    index: Int,
-    size: Int,
-    verticalSpace: Dp,
-    horizontalSpace: Dp,
-    itemContent: @Composable () -> Unit,
-    footer: (@Composable () -> Unit)? = null,
-    onClickFooter: (() -> Unit)? = null,
-    header: (@Composable () -> Unit)? = null,
-    onClickHeader: (() -> Unit)? = null,
-) {
-    Box(
-        modifier = Modifier
-            .wrapContentWidth()
-            .padding(
-                paddingValues = PaddingValues(
-                    horizontal = horizontalSpace,
-                    vertical = verticalSpace,
-                )
-            )
-            .quackClickable(
-                onClick = onClickFooter ?: onClickHeader,
-                rippleEnabled = true,
-            ),
-        contentAlignment = Alignment.Center,
     ) {
-        if (index == 0 && header != null) {
-            header()
-        } else if (index == size - 1 && footer != null) {
-            footer()
-        } else {
-            itemContent()
+        itemsIndexed(
+            items = items,
+            key = key,
+            span = span,
+            contentType = contentType,
+        ) { index, item ->
+            itemContent(
+                index = index,
+                item = item,
+            )
         }
     }
 }
