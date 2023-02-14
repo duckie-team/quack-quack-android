47332f738ee397d3e85dc7774730e303c169b64a
Ji Sungbin <ji@sungb.in>
2023-02-15 01:48:29 +0900

    디자인 컴포넌트 개발 기초 준비 (#459)
    * feat: 컴포넌트 기본 재료 구성
    
    * style: AllowMagicNumber 어노테이션 지원 추가
    
    * chore: 사용하지 않는 모듈 빌드 제거
    
    * style: 누락된 trailing comma 추가

---
+++ core/src/main/kotlin/team/duckie/quackquack/core/util/string.kt
@@ -0,0 +1,15 @@
+/*
+ * Designed and developed by Duckie Team, 2022~2023
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ */
+
+@file:Suppress("unused")
+
+package team.duckie.quackquack.core.util
+
+/**
+ * 공백 상태를 도메인적으로 나타내기 위해 사용됩니다.
+ */
+public inline val String.Companion.Empty: String get() = ""
