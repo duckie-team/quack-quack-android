c4e81c60f5e7b505a61c130747be86b4bdeafd82
jisungbin <ji@sungb.in>
2023-02-16 17:37:19 +0900

    feat: 임시 MainActivity 구성

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -5,6 +5,18 @@
  * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
  */
 
+plugins {
+    `android-application`
+}
+
 GradleInstallation.with(project) {
     android(namespace = "team.duckie.quackquack.playground")
+    compose()
+}
+
+dependencies {
+    implementations(
+        libs.compose.activity,
+        projects.core,
+    )
 }
--- playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
@@ -7,6 +7,31 @@
 
 package team.duckie.quackquack.playground
 
-import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.Modifier
+import team.duckie.quackquack.core.component.QuackText
+import team.duckie.quackquack.core.material.QuackTypography
+import team.duckie.quackquack.core.theme.QuackTheme
 
-class MainActivity : Activity()
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuackTheme {
+                QuackText(
+                    modifier = Modifier.highlight(
+                        highlights = listOf(
+                            "one" to { },
+                            "two" to { },
+                            "three" to { },
+                        ),
+                    ),
+                    text = "one two three four five",
+                    typography = QuackTypography.Body1,
+                )
+            }
+        }
+    }
+}
