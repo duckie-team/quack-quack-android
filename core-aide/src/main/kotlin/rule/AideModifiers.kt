7bbe0432bb925f3b9c53956e53c1c39ba7c0804d
jisungbin <ji@sungb.in>
2023-03-06 02:37:42 +0900

    feat: keep up-to-date aide rules

--- core-aide/src/main/kotlin/rule/AideModifiers.kt
+++ core-aide/src/main/kotlin/rule/AideModifiers.kt
@@ -2,18 +2,13 @@
 // Do not modify it arbitrarily.
 @file:Suppress("NoConsecutiveBlankLines", "PackageDirectoryMismatch", "ktlint")
 
-import kotlin.String
-import kotlin.Suppress
-import kotlin.collections.List
-import kotlin.collections.Map
-
 internal val aideModifiers: Map<String, List<String>> = run {
   val aide = mutableMapOf<String, List<String>>()
+
   aide["text"] = listOf("span", "highlight")
-  // --- START: text Modifiers ---
   aide["span"] = emptyList()
   aide["highlight"] = emptyList()
-  // --- END: text Modifiers ---
+
   aide
 }
 
