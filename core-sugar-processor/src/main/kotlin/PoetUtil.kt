bbd38731aad062f5a7eb3fe75e3a69720e98def7
jisungbin <ji@sungb.in>
2023-03-13 03:51:56 +0900

    docs: update copyright comments

--- core-sugar-processor/src/main/kotlin/PoetUtil.kt
+++ core-sugar-processor/src/main/kotlin/PoetUtil.kt
@@ -1,8 +1,8 @@
 /*
- * Designed and developed by Duckie Team, 2022~2023
+ * Designed and developed by Duckie Team 2023.
  *
  * Licensed under the MIT.
- * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/2.x.x/LICENSE
  */
 
 import com.squareup.kotlinpoet.FileSpec
