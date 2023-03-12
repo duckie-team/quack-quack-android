c8fe087c936f6fcc3369b0980f0b36600fefcada
jisungbin <ji@sungb.in>
2023-03-13 07:13:11 +0900

    feat: update aide codes

--- core-aide/src/main/kotlin/rule/AideComponents.kt
+++ core-aide/src/main/kotlin/rule/AideComponents.kt
@@ -1,6 +1,11 @@
 // This file was automatically generated. Do not modify it arbitrarily.
 @file:Suppress("NoConsecutiveBlankLines", "PackageDirectoryMismatch", "ktlint")
 
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.Map
+
 internal val aideComponents: Map<String, List<String>> = run {
   val aide = mutableMapOf<String, List<String>>()
   aide["QuackAnimatedContent"] = listOf("QuackAnimatedContent")
