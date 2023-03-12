7477bb1c1328a148df902e5e3b5de0d19f64a77a
jisungbin <ji@sungb.in>
2023-03-12 22:49:51 +0900

    chore: versioning task 등록

---
+++ buildSrc/src/main/kotlin/internal/versioning.kt
@@ -0,0 +1,24 @@
+/*
+ * Designed and developed by Duckie Team, 2022~2023
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/duckie-quack-quack/blob/main/LICENSE
+ */
+
+package internal
+
+import java.io.File
+import org.gradle.api.Project
+
+internal fun Project.parseArtifactVersion(): String {
+    val versionFile = File(projectDir, "version.txt")
+    if (!versionFile.exists() || !versionFile.isFile) {
+        error(
+            """
+            There is no version.txt file in the project path. 
+            Use `./gradlew :$name:versioning -Ptype=init` for version configuration.
+            """.trimIndent(),
+        )
+    }
+    return versionFile.readText()
+}
