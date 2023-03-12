778ad4a122b003f81b6f3224aeb2cefa9e6023d2
jisungbin <ji@sungb.in>
2023-03-12 20:12:54 +0900

    feat: QuackArtifactType 구성 추가

--- buildSrc/src/main/kotlin/QuackMavenPublishingPlugin.kt
+++ buildSrc/src/main/kotlin/QuackMavenPublishingPlugin.kt
@@ -13,3 +13,78 @@ class QuackMavenPublishingPlugin : Plugin<Project> {
         TODO("Not yet implemented")
     }
 }
+
+open class QuackMavenExtension {
+    open lateinit var type: QuackArtifactType
+
+    internal val isInitialized get() = ::type.isInitialized
+
+    override fun toString() = """
+        ## Debug
+        artifactId: ${type.artifactId}
+        description: ${type.description}
+        level: ${type.level}
+        
+        ## toString
+        artifactId: $type
+    """.trimIndent()
+}
+
+enum class QuackArtifactType(
+    internal val artifactId: String,
+    internal val description: String,
+    internal val level: ArtifactLevel = ArtifactLevel.Public,
+) {
+    BOM(
+        artifactId = "quackquack-bom",
+        description = "Duckie's design system artifacts BOM",
+    ),
+    CommonKotlinPoet(
+        artifactId = "quackquack-common-kotlinpoet",
+        description = "Utils used by QuackQuack for KotlinPoet",
+        level = ArtifactLevel.Internal,
+    ),
+    Core(
+        artifactId = "quackquack-core",
+        description = "The UI components of the Duckie design system",
+    ),
+    CoreAide(
+        artifactId = "quackquack-core-aide",
+        description = "Design convention lint for the Duckie design system",
+    ),
+    CoreAideAnnotation(
+        artifactId = "quackquack-core-aide-annotation",
+        description = "Marker annotations for CoreAide",
+    ),
+    CoreAideProcessor(
+        artifactId = "quackquack-core-aide-processor",
+        description = "Kotlin Symbol Processing for CoreAideAnnotation",
+        level = ArtifactLevel.Internal,
+    ),
+    CoreSugar(
+        artifactId = "quackquack-core-sugar",
+        description = "A sugar syntax for the Duckie design system",
+    ),
+    CoreSugarAnnotation(
+        artifactId = "quackquack-core-sugar-annotation",
+        description = "Marker annotations for CoreSugar",
+    ),
+    CoreSugarProcessor(
+        artifactId = "quackquack-core-sugar-processor",
+        description = "Kotlin Symbol Processing for CoreSugarAnnotation",
+        level = ArtifactLevel.Internal,
+    ),
+    DokkaPaparazziIntegrate(
+        artifactId = "dokka-paparazzi-integrate",
+        description = "DokkaPlugin to integrate Dokka and Paparazzi",
+        level = ArtifactLevel.Internal,
+    );
+
+    override fun toString(): String {
+        TODO()
+    }
+}
+
+enum class ArtifactLevel {
+    Public, Internal,
+}
