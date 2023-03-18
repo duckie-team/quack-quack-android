a2dee2535301e8e8c21ff4277973ad09effb2a72
jisungbin <ji@sungb.in>
2023-03-18 23:56:20 +0900

    add sugar annotation stubs

--- core-sugar-processor-kotlinc/src/test/kotlin/stubs.kt
+++ core-sugar-processor-kotlinc/src/test/kotlin/stubs.kt
@@ -29,4 +29,65 @@ val stubs = arrayOf(
         annotation class Composable
         """,
     ),
+    kotlin(
+        "annotations.kt",
+        """
+        package team.duckie.quackquack.sugar.material
+        
+        import kotlin.reflect.KClass
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarName(val name: String = DEFAULT_NAME) {
+            companion object {
+                const val PREFIX_NAME: String = "Quack"
+                const val DEFAULT_NAME: String = "<<DEFAULT_NAME>>"
+                const val TOKEN_NAME: String = "<<SUGAR_TOKEN>>"
+            }
+        }
+        
+        @Target(AnnotationTarget.VALUE_PARAMETER)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarToken
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarRefer(val fqn: String)
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class NoSugar
+        
+        @Target(AnnotationTarget.FILE)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class GeneratedFile
+        
+        @Target(AnnotationTarget.VALUE_PARAMETER)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class Imports(vararg val clazz: KClass<*>)
+        """,
+    ),
+    kotlin(
+        "optin.kt",
+        """
+        package team.duckie.quackquack.sugar.material
+
+        @RequiresOptIn(
+            message = "This indicates that the feature should only be used in the Sugar Compiler. " +
+                    "Using it arbitrarily can lead to unexpected bugs.",
+        )
+        @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarCompilerApi
+        """,
+    ),
+    kotlin(
+        "typer.kt",
+        """
+        package team.duckie.quackquack.sugar.material
+        fun <T> sugar(): T {
+            throw NotImplementedError("TODO: provide message")
+        }   
+        """,
+    ),
 )
