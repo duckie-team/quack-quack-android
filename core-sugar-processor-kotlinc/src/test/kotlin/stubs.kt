1dfe168b0b67ee758215d379c763a63851e732f1
jisungbin <ji@sungb.in>
2023-03-18 23:56:20 +0900

    add sugar annotation stubs

--- core-sugar-processor-kotlinc/src/test/kotlin/stubs.kt
+++ core-sugar-processor-kotlinc/src/test/kotlin/stubs.kt
@@ -29,4 +29,42 @@ val stubs = arrayOf(
         annotation class Composable
         """,
     ),
+    kotlin(
+        "sugar-annotations.kt",
+        """
+        package team.duckie.quackquack.sugar.material
+        
+        import kotlin.reflect.KClass
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarName(val name: String = DEFAULT_NAME) {
+            companion object {
+                const val PREFIX_NAME: String = "Quack"
+                const val DEFAULT_NAME: String = "<<DEFAULT_NAME>>"
+                const val TOKEN_NAME: String = "<<SUGAR_TOKEN>>"
+            }
+        }
+        
+        @Target(AnnotationTarget.VALUE_PARAMETER)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarToken
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class SugarRefer(val fqn: String)
+        
+        @Target(AnnotationTarget.FUNCTION)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class NoSugar
+        
+        @Target(AnnotationTarget.FILE)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class GeneratedFile
+        
+        @Target(AnnotationTarget.VALUE_PARAMETER)
+        @Retention(AnnotationRetention.BINARY)
+        annotation class Imports(vararg val clazz: KClass<*>)
+        """,
+    ),
 )
