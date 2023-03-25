0210ea24f650f03ca3856deba1729c8c127d1970
jisungbin <ji@sungb.in>
2023-03-26 08:52:30 +0900

    [WIP] core-aide testing

--- core-aide/src/test/kotlin/ModifierInformationalTest.kt
+++ core-aide/src/test/kotlin/ModifierInformationalTest.kt
@@ -8,23 +8,68 @@
 @file:Suppress("TrailingCommaOnCallSite")
 
 import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FreeSpec
 
-class ModifierInformationalTest : StringSpec({
-    "허용되지 않은 데코레이터를 사용했을 때 informational issue가 발생함" {
-        lintTest(
-            kotlin(
-                "text.kt",
-                """
-                fun main() {
-                    QuackText(modifier = Modifier.onClick {})
-                }
-                """,
-            ),
-        )
-            .expectClean()
+class ModifierInformationalTest : FreeSpec({
+    "허용되지 않은 데코레이터를 사용했을 때 informational issue가 발생함" - {
+        "Modifier.Companion" {
+            lintTest(
+                kotlin(
+                    "main.kt",
+                    """
+                    fun main() {
+                        QuackText(Modifier.span("", 0).onClick {}.span("", 0).onClick {}.onClick {})
+                        QuackText(modifier = Modifier.span("", 0).onClick {}.span("", 0).onClick {}.onClick {})
+                    }
+                    """,
+                ),
+            )
+                .expectClean()
+        }
+
+        "Modifier".config(enabled = false) {
+            lintTest(
+                kotlin(
+                    "text.kt",
+                    """
+                    fun main() {
+                        QuackText(modifier = Modifier.onClick {})
+                    }
+                    """,
+                ),
+            )
+                .expectClean()
+        }
+
+        "별도의 변수로 분리된 Modifier".config(enabled = false) {
+            lintTest(
+                kotlin(
+                    "main.kt",
+                    """
+                    fun main() {
+                        val modifier = Modifier.span("", 1).onClick {}.span("", 2)
+                        QuackText(modifier)
+                        QuackText(modifier = modifier)
+
+                        val modifier2 = Modifier
+                            .span("", 1)
+                            .longParameters(
+                                a = 1,
+                                b = 2,
+                                c = 3,
+                                lambda = {},
+                            )
+                            .onClick {}
+                            .span("", 2)
+                        QuackText(modifier)
+                        QuackText(modifier = modifier)
+                    }
+                    """.trimIndent()
+                )
+            )
+        }
     }
 
-    "informational issue가 발생했을 때 유효한 QuickFix가 제공됨".config(enabled = false) {
+    "informational issue가 발생했을 때 유효한 QuickFix가 제공됨".config(enabled = false) - {
     }
 })
