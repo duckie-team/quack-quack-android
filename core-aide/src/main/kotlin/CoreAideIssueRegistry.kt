3a2927d82f12baa007deb5f9b50805bca29036d5
jisungbin <ji@sungb.in>
2023-03-26 02:16:41 +0900

    CoreAideIssueRegistry cleanup

--- core-aide/src/main/kotlin/CoreAideIssueRegistry.kt
+++ core-aide/src/main/kotlin/CoreAideIssueRegistry.kt
@@ -5,25 +5,15 @@
  * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/2.x.x/LICENSE
  */
 
-@file:Suppress("unused")
-
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
-import com.android.tools.lint.detector.api.describeApi
 import com.google.auto.service.AutoService
 
-@AutoService(value = [IssueRegistry::class])
+@AutoService(IssueRegistry::class)
 class CoreAideIssueRegistry : IssueRegistry() {
     override val issues = listOf(CoreAideDecorateModifierDetector.ISSUE)
-
     override val api = CURRENT_API
-
-    /**
-     * works with Studio 4.0 or later; see [describeApi].
-     */
-    override val minApi = 7
-
     override val vendor = Vendor(
         vendorName = "DUCKIE",
         identifier = "duckie-team/quack-quack-android",
