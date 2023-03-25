e29532c6250c9eab7d7a06cfedbc1dbf5d82bc28
jisungbin <ji@sungb.in>
2023-03-26 02:16:25 +0900

    Improve documents

--- core-aide/src/main/kotlin/CoreAideDecorateModifierDetector.kt
+++ core-aide/src/main/kotlin/CoreAideDecorateModifierDetector.kt
@@ -26,19 +26,19 @@ import org.jetbrains.uast.UExpression
 import org.jetbrains.uast.getQualifiedChain
 
 /**
- * 꽥꽥 컴포넌트가 해당 도메인에 맞지 않는 Modifier를 사용하면 경고를 표시합니다.
+ * 꽥꽥 컴포넌트가 해당 도메인에 맞지 않는 DecorateModifier를 사용하면 경고를 표시합니다.
  *
  * ```kotlin
  * fun main() {
  *   QuackText(
  *     modifier = Modifier.longClick {}, // warning!
- *     text = "Modifier#longClick is Button domain modifier.",
+ *     text = "Modifier#longClick is Button domain DecorateModifier.",
  *   )
  * }
  * ```
  *
- * 도메인별 꽥꽥 컴포넌트와 도메인별 사용 가능한 Modifier는 core-aide-processor에 의해
- * 자동 생성됩니다. 해당 파일들은 rule 패키지에 저장되며, [`aideModifiers`][aideModifiers]와 [`aideComponents`][aideComponents]로
+ * 도메인별 꽥꽥 컴포넌트와 도메인별 사용 가능한 DecorateModifier는 core-aide-processor에 의해
+ * 자동 생성됩니다. 해당 파일들은 rule 패키지에 저장되며, [`aideModifiers`][aideModifiers]와 [`quackComponents`][quackComponents]로
  * 조회할 수 있습니다.
  */
 class CoreAideDecorateModifierDetector : Detector(), SourceCodeScanner {
@@ -52,7 +52,7 @@ class CoreAideDecorateModifierDetector : Detector(), SourceCodeScanner {
             val chains = argument.getQualifiedChain()
             val isModifier = chains.firstOrNull()?.let { chain ->
                 val type = chain.getExpressionType()?.canonicalText
-                type?.startsWith("androidx.compose.ui.Modifier") == true // `Modifier` or `Modifier.Companion` type
+                type?.startsWith(ModifierFqn) == true // `Modifier` or `Modifier.Companion` type
             } == true
             isModifier to chains.drop(1) // without `modifier` or `Modifier` chain
         } ?: return
@@ -113,11 +113,11 @@ class CoreAideDecorateModifierDetector : Detector(), SourceCodeScanner {
     }
 
     companion object {
-        private const val IssueId = "DecorateModifier"
-        private const val IssueMessage = "올바르지 않은 Modifier의 사용이 감지되었습니다."
-        private const val IssueBrief = "사용하고자 하는 Modifier가 해당 스코프에 선언된 Modifier가 맞나요?"
+        private const val IssueId = "WrongDecorateModifier"
+        private const val IssueMessage = "올바르지 않은 DecorateModifier의 사용이 감지되었습니다."
+        private const val IssueBrief = "사용하고자 하는 DecorateModifier가 해당 스코프에 선언된 게 맞나요?"
         private val IssueExplanation = """
-            꽥꽥은 컴포넌트를 도메인으로 구분하며, 도메인마다 사용 가능한 Modifier가 달라집니다.
+            꽥꽥은 컴포넌트를 도메인으로 구분하며, 도메인마다 사용 가능한 DecorateModifier가 달라집니다.
             예를 들어 다음과 같은 컴포넌트가 있습니다.
             
             ```
@@ -128,10 +128,9 @@ class CoreAideDecorateModifierDetector : Detector(), SourceCodeScanner {
             ```
             
             위 `QuackText` 컴포넌트는 텍스트 표시가 주 목적이므로 Text 도메인으로 간주됩니다. 
-            일부 Modifier는 Text 도메인에 속하는 컴포넌트에만 사용됨을 기준으로 설계됐기 때문에 
+            모든 DecorateModifier는 해당 도메인에 속하는 컴포넌트에만 사용됨을 기준으로 설계됐기 때문에 
             외부 도메인에서는 올바르게 작동하지 않을 수 있습니다. 따라서 도메인에 맞지 않는 
-            Modifier 사용이 감지됐을 때는 `$IssueId` informational을 발생시킵니다.
-            ```
+            DecorateModifier 사용이 감지됐을 때는 `$IssueId` informational을 발생시킵니다.
         """.trimIndent()
 
         val ISSUE = Issue.create(
