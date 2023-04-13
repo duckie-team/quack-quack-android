7fd9efb720e754c28820f22cce899c6a1c25e076
jisungbin <ji@sungb.in>
2023-04-14 04:29:52 +0900

    [WIP] casa playground

--- playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
+++ playground/src/main/kotlin/team/duckie/quackquack/playground/MainActivity.kt
@@ -14,79 +14,89 @@ import androidx.compose.runtime.remember
 import kotlinx.collections.immutable.persistentListOf
 import team.duckie.quackquack.casa.material.CasaModel
 import team.duckie.quackquack.casa.ui.CasaScreen
+import team.duckie.quackquack.casa.ui.theme.CasaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val testCasaModels = remember {
-                persistentListOf(
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "one",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "two",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "three",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "four",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "five",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "five",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "five",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "five",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                    CasaModel(
-                        name = "one",
-                        path = "",
-                        domain = "five",
-                        kdocDefaultSection = "kdoc",
-                        components = persistentListOf(),
-                    ),
-                )
+            CasaTheme {
+                val testCasaModels = remember {
+                    persistentListOf(
+                        CasaModel(
+                            name = "one",
+                            path = "",
+                            domain = "one",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "two",
+                            path = "",
+                            domain = "two",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "three",
+                            path = "",
+                            domain = "three",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "four",
+                            path = "",
+                            domain = "four",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "five",
+                            path = "",
+                            domain = "five",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "six",
+                            path = "",
+                            domain = "six",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "seven",
+                            path = "",
+                            domain = "seven",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "eight",
+                            path = "",
+                            domain = "eight",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "nine",
+                            path = "",
+                            domain = "nine",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                        CasaModel(
+                            name = "ten",
+                            path = "",
+                            domain = "ten",
+                            kdocDefaultSection = "kdoc",
+                            components = persistentListOf(),
+                        ),
+                    )
+                }
+                CasaScreen(models = testCasaModels)
             }
-            CasaScreen(models = testCasaModels)
         }
     }
 }
