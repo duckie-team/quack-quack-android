6181ad47ca53fc8b90a1c500ace0115008d2711a
jisungbin <ji@sungb.in>
2023-04-25 04:47:39 +0900

    Fix "FIXME"

--- runtime/src/main/kotlin/team/duckie/quackquack/runtime/materializing.kt
+++ runtime/src/main/kotlin/team/duckie/quackquack/runtime/materializing.kt
@@ -8,10 +8,9 @@
 package team.duckie.quackquack.runtime
 
 import android.annotation.SuppressLint
-import androidx.compose.runtime.Composable
+import androidx.annotation.VisibleForTesting
 import androidx.compose.runtime.Composer
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import team.duckie.quackquack.util.MustBeTested
@@ -46,21 +45,12 @@ public class QuackMaterializeResult internal constructor(
     public operator fun component2(): List<QuackDataModifierModel> = quackDataModels
 }
 
-@JvmInline
-@Immutable
-private value class QuackMaterializableComposedModifier(
-    val factory: @Composable Modifier.() -> Modifier,
-) : Modifier.Element
-
-/**
- * [quackMaterializeOf]에서 `ComposedModifier`를 처리하기 위한 클래스입니다.
- *
- * `composed`의 결과가 [QuackDataModifierModel]일 경우 `acc`를 유지하며 folding하기 위해
- * `ComposedModifier`의 별도 대응이 필요합니다.
- */
-@Stable
-public fun Modifier.quackComposed(factory: @Composable Modifier.() -> Modifier): Modifier {
-    return then(QuackMaterializableComposedModifier(factory))
+@VisibleForTesting
+internal object QuackMaterializingErrors {
+    const val QuackDataModelProducerButNot =
+        "The result of the Modifier factory was expected to be a QuackDataModifierModel " +
+                "because isQuackDataModelProducer was provided as true, but it is not. " +
+                "Please provide isQuackDataModelProducer as false for correct information. "
 }
 
 /**
@@ -78,7 +68,7 @@ public fun Composer.quackMaterializeOf(
     modifier: Modifier,
     taversingCallback: (modifier: Modifier) -> Unit = {},
 ): QuackMaterializeResult {
-    val needsNewGroup = modifier.any { it is QuackMaterializableComposedModifier }
+    val needsNewGroup = modifier.any { it is QuackComposedModifier }
 
     // Random number for fake group key. Chosen by fair die roll.
     // `Integer.toHexString(Random.nextInt())`
@@ -92,15 +82,16 @@ public fun Composer.quackMaterializeOf(
                 quackDataModels += element
                 acc
             }
-            is QuackMaterializableComposedModifier -> {
-                @Suppress("UNCHECKED_CAST")
-                val factory = element.factory as Modifier.(Composer, Int) -> Modifier
-                val composed = factory(Modifier, this, 0)
-                if (composed is QuackDataModifierModel) {
+            is QuackComposedModifier -> {
+                if (element.quackDataModelProducer) {
+                    @Suppress("UNCHECKED_CAST")
+                    val factory = element.factory as Modifier.(Composer, Int) -> Modifier
+                    val composed = factory.invoke(Modifier, this, 0) as? QuackDataModifierModel ?: error(
+                        QuackMaterializingErrors.QuackDataModelProducerButNot,
+                    )
                     quackDataModels += composed
                     acc
                 } else {
-                    // FIXME: 최초 컴포지션시에 stdlib-materializerOf으로 들어가면 re-invoke됨 (동일 프레임에서)
                     @Suppress("UnnecessaryComposedModifier")
                     acc.composed(factory = element.factory)
                 }
