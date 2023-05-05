a9f778622d7c7b3bf93673122d319548c88842dc
jisungbin <ji@sungb.in>
2023-05-05 18:35:37 +0900

    [WIP] Writing ui tests for Button components

--- ui/src/androidTest/kotlin/team/duckie/quackquack/ui/component/ButtonTest.kt
+++ ui/src/androidTest/kotlin/team/duckie/quackquack/ui/component/ButtonTest.kt
@@ -5,11 +5,14 @@
  * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
  */
 
+@file:OptIn(ExperimentalQuackQuackApi::class)
+
 package team.duckie.quackquack.ui.component
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
 import org.junit.Test
+import team.duckie.quackquack.ui.util.ExperimentalQuackQuackApi
 
 class ButtonTest {
     @get:Rule
@@ -18,60 +21,60 @@ class ButtonTest {
     // `QuackLargeButtonDefaults` 스타일이 골든 디자인과 일치해야 함
     @Test
     fun QuackLargeButtonDefaults_style_match_golden_design() {
-
+        // TODO: golden image
     }
 
     // `QuackMediumButtonDefaults` 스타일이 골든 디자인과 일치해야 함
     @Test
     fun QuackMediumButtonDefaults_style_match_golden_design() {
-
+        // TODO: golden image
     }
 
     // `QuackSmallButtonDefaults` 스타일이 골든 디자인과 일치해야 함
     @Test
     fun QuackSmallButtonDefaults_style_match_golden_design() {
-
+        // TODO: golden image
     }
 
     // `QuackLargeButtonDefaults` 스타일에 커스텀 프로퍼티를 적용할 수 있어야 함
     @Test
     fun QuackLargeButtonDefaults_style_with_custom_properties() {
-
+        // TODO: golden image
     }
 
     // `QuackMediumButtonDefaults` 스타일에 커스텀 프로퍼티를 적용할 수 있어야 함
     @Test
     fun QuackMediumButtonDefaults_style_with_custom_properties() {
-
+        // TODO: golden image
     }
 
     // `QuackSmallButtonDefaults` 스타일에 커스텀 프로퍼티를 적용할 수 있어야 함
     @Test
     fun QuackSmallButtonDefaults_style_with_custom_properties() {
-
+        // TODO: golden image
     }
 
     // `enabled`가 false일 때는 `onClick` 이벤트가 작동하면 안됨
     @Test
     fun onClick_shouldnt_works_when_enabled_is_false() {
-
+        
     }
 
     // 직접 사이즈를 지정했을 경우에는 `contentPadding`이 적용되면 안됨
     @Test
     fun contentPadding_shouldnt_works_when_size_specified() {
-
+        // TODO: golden image
     }
 
     // `contentPadding`은 버튼 텍스트를 기준으로 적용돼야 함
     @Test
     fun contentPadding_works_with_text_baseline() {
-
+        // TODO: golden image
     }
 
     // `QuackSmallButtonDefaults` 스타일은 데코레이터가 적용되면 안됨
     @Test
     fun QuackSmallButtonDefaults_style_shouldnt_accept_decorate() {
-
+        // TODO: golden image
     }
 }
