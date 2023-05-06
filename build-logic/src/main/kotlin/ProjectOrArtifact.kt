e8820de3d7dda6cc1834b129e1358c42cb816d2c
jisungbin <ji@sungb.in>
2023-05-06 09:29:26 +0900

    Write missing documents

--- build-logic/src/main/kotlin/ProjectOrArtifact.kt
+++ build-logic/src/main/kotlin/ProjectOrArtifact.kt
@@ -7,7 +7,12 @@
 
 import org.gradle.api.artifacts.ProjectDependency
 
-// TODO: 문서화
+/**
+ * 로컬 모듈을 publishing된 아티팩트로 변환합니다.
+ * 해당 모듈이 maven-central에 publishing 되어 있다고 가정합니다.
+ *
+ * @see useArtifact
+ */
 fun ProjectDependency.orArtifact(): Any {
     return if (useArtifact) {
         val artifact = ArtifactConfig.of(dependencyProject)
