fd36dde0acd71a720f7554e9d28c38ebbd21dd30
jisungbin <ji@sungb.in>
2023-05-07 19:39:44 +0900

    Cleanup testing

---
+++ util-backend-test/build.gradle.kts
@@ -0,0 +1,11 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
+plugins {
+    quackquack("jvm-kotlin")
+    quackquack("kotlin-explicit-api")
+}
---
+++ util-backend-test/src/main/kotlin/team/duckie/quackquack/util/backend/test/stub/casa.kt
@@ -0,0 +1,25 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.util.backend.test.stub
+
+import org.intellij.lang.annotations.Language
+
+public object CasaStub {
+    @Language("kotlin")
+    public const val Annotations: String = """
+package team.duckie.quackquack.casa.annotation
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class Casa
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
+annotation class CasaValue(val literal: String)
+    """
+}
