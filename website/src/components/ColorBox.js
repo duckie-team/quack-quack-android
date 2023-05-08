58f63f160c80656146940ab9d9b4c47f504b1209
jisungbin <ji@sungb.in>
2023-05-08 13:59:18 +0900

    Change indentation level to space 2

--- website/src/components/ColorBox.js
+++ website/src/components/ColorBox.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default function ColorBox({width, height, color}) {
-    const style = {
-        width: width,
-        height: height,
-        'background-color': color,
-        display: "inline-block",
-    };
+  const style = {
+    width: width,
+    height: height,
+    'background-color': color,
+    display: "inline-block",
+  };
 
-    return (
-        <div style={style}/>
-    );
+  return (
+    <div style={style}/>
+  );
 }
