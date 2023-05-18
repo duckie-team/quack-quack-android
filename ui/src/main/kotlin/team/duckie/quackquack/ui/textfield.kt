54c505c313d4538abaed5e71b2f1307cad13b700
jisungbin <ji@sungb.in>
2023-05-19 00:27:06 +0900

    Add QuackTextField data models

--- ui/src/main/kotlin/team/duckie/quackquack/ui/textfield.kt
+++ ui/src/main/kotlin/team/duckie/quackquack/ui/textfield.kt
@@ -7,3 +7,50 @@
 
 package team.duckie.quackquack.ui
 
+import androidx.annotation.IntRange
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.unit.Dp
+import team.duckie.quackquack.material.QuackColor
+import team.duckie.quackquack.material.QuackIcon
+import team.duckie.quackquack.material.QuackTypography
+import team.duckie.quackquack.runtime.QuackDataModifierModel
+import team.duckie.quackquack.ui.token.HorizontalDirection
+import team.duckie.quackquack.ui.token.IconRole
+import team.duckie.quackquack.ui.token.VerticalDirection
+
+@Immutable
+public enum class TextFieldState {
+  Error, Success, Default,
+}
+
+@Immutable
+public enum class PlaceholderStrategy {
+  Always, Hidable,
+}
+
+@Stable
+private data class TextFieldIconData(
+  val icon: QuackIcon,
+  val tint: QuackColor,
+  val tintGetter: ((text: String) -> QuackColor)?,
+  val role: IconRole,
+  val direction: HorizontalDirection,
+  val onClick: ((text: String) -> Unit)?,
+) : QuackDataModifierModel
+
+@Stable
+private data class TextFieldBorderData(
+  val thickness: Dp,
+  val color: QuackColor,
+  val direction: VerticalDirection,
+) : QuackDataModifierModel
+
+@Stable
+private data class TextFieldCountableData(
+  val enable: Boolean,
+  val baseColor: QuackColor,
+  val highlightColor: QuackColor,
+  val typography: QuackTypography,
+  @IntRange(from = 1) val maxCount: Int,
+) : QuackDataModifierModel
---
+++ ui/src/main/kotlin/team/duckie/quackquack/ui/token/direction.kt
@@ -0,0 +1,16 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.token
+
+public enum class HorizontalDirection {
+  Left, Right,
+}
+
+public enum class VerticalDirection {
+  Top, Down,
+}
