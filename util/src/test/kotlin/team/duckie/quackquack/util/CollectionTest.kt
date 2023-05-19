3712bc2c3d0323848d423cbf7699029ad648d5da
jisungbin <ji@sungb.in>
2023-05-20 06:07:32 +0900

    Add fastFilterIsInstanceOrNull to the collection utility

--- util/src/test/kotlin/team/duckie/quackquack/util/CollectionTest.kt
+++ util/src/test/kotlin/team/duckie/quackquack/util/CollectionTest.kt
@@ -8,18 +8,19 @@
 package team.duckie.quackquack.util
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldBeTypeOf
 
 class CollectionTest : FreeSpec() {
   init {
     "fastFirstIsInstanceOrNul" - {
-      "반환된 요소는 T로 캐스팅 가능함" {
+      "캐스팅 가능한 요소가 있다면 null이 반환되지 않음" {
         val elements = listOf(1, 'Q', Float, "4", Any())
         val stringElement = elements.fastFirstIsInstanceOrNull<String>()
 
-        stringElement.shouldBeTypeOf<String>()
+        stringElement.shouldNotBeNull()
       }
 
       "null를 반환하면 주어진 리스트에 존재하지 않음" {
@@ -34,6 +35,28 @@ class CollectionTest : FreeSpec() {
         val stringElement = elements.fastFirstIsInstanceOrNull<String>()
 
         stringElement shouldBe "4"
+      }
+    }
+    "fastFilterIsInstanceOrNull" - {
+      "캐스팅 가능한 요소가 있다면 null이 반환되지 않음" {
+        val elements = listOf(1, 'Q', Float, "4", Any(), "10")
+        val stringElement = elements.fastFilterIsInstanceOrNull<String>()
+
+        stringElement.shouldNotBeNull()
+      }
+
+      "null를 반환하면 주어진 리스트에 존재하지 않음" {
+        val elements = listOf(1, 'Q', Float, "4", Any())
+        val stringElement = elements.fastFilterIsInstanceOrNull<Double>()
+
+        stringElement.shouldBeNull()
+      }
+
+      "여러개의 요소가 제공됐다면 해당되는 모든 요소가 반환됨" {
+        val elements = listOf(1, 'Q', Float, "4", Any(), "6")
+        val stringElement = elements.fastFilterIsInstanceOrNull<String>()
+
+        stringElement.shouldContainExactly("4", "6")
       }
     }
   }
