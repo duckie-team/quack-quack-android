c793d86b32be4e2b0707e469e15f980c7db4d2a4
jisungbin <ji@sungb.in>
2023-05-24 18:27:31 +0900

    Prepare snapshot test

--- ui/src/test/kotlin/team/duckie/quackquack/ui/snapshot/util/SnapshotPathGenerator.kt
+++ ui/src/test/kotlin/team/duckie/quackquack/ui/snapshot/util/SnapshotPathGenerator.kt
@@ -10,15 +10,31 @@
 package team.duckie.quackquack.ui.snapshot.util
 
 import java.io.File
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
 const val BaseSnapshotPath = "src/test/snapshots"
 
 inline fun snapshotPath(domain: String, isGif: Boolean = false): File {
   return File("$BaseSnapshotPath/$domain/${getCurrentMethodName()}.${if (isGif) "gif" else "png"}")
-    .also { file -> file.parentFile?.mkdirs() }
 }
 
 inline fun getCurrentMethodName(): String {
   // https://stackoverflow.com/a/32329165/14299073
   return Thread.currentThread().stackTrace[1].methodName
+}
+
+class SnapshotPathGeneratorRule(private val domain: String) : TestWatcher() {
+  init {
+    File("$BaseSnapshotPath/$domain").mkdirs()
+  }
+
+  private var realtimeTestMethodName: String? = null
+
+  override fun starting(description: Description) {
+    realtimeTestMethodName = description.methodName
+  }
+
+  operator fun invoke(isGif: Boolean = false): File =
+    File("$BaseSnapshotPath/$domain/$realtimeTestMethodName.${if (isGif) "gif" else "png"}")
 }
