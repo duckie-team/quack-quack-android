966340627d3e3a4e999ea019905b915302544b6b
jisungbin <ji@sungb.in>
2023-05-24 09:03:01 +0900

    Add contentDescription for QuackIcon

--- ui/src/main/kotlin/team/duckie/quackquack/ui/icon.kt
+++ ui/src/main/kotlin/team/duckie/quackquack/ui/icon.kt
@@ -16,12 +16,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import team.duckie.quackquack.material.QuackColor
 import team.duckie.quackquack.material.QuackIcon
 import team.duckie.quackquack.sugar.material.SugarToken
 import team.duckie.quackquack.ui.util.fontScaleAwareIconSize
+import team.duckie.quackquack.util.applyIf
 
 /**
  * [QuackIcon]을 그립니다. 아이콘 사이즈는 [Modifier.size]로 정하는 게 아닌
@@ -41,6 +44,7 @@ public fun QuackIcon(
   size: Dp = 24.dp,
   tint: QuackColor = QuackColor.Unspecified,
   contentScale: ContentScale = ContentScale.Fit,
+  contentDescription: String? = null,
 ) {
   val currentColorFilter = remember(tint) { tint.toColorFilterOrNull() }
 
@@ -52,6 +56,11 @@ public fun QuackIcon(
         painter = icon.asPainter(),
         contentScale = contentScale,
         colorFilter = currentColorFilter,
-      ),
+      )
+      .applyIf(contentDescription != null) {
+        semantics {
+          this.contentDescription = contentDescription!!
+        }
+      },
   )
 }
