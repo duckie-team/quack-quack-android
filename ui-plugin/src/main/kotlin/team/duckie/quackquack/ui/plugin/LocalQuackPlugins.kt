3bc6d9c2683b7b404e5edfd839af25d8d65bead1
jisungbin <ji@sungb.in>
2023-05-27 17:32:26 +0900

    ui-plugin documentation

--- ui-plugin/src/main/kotlin/team/duckie/quackquack/ui/plugin/LocalQuackPlugins.kt
+++ ui-plugin/src/main/kotlin/team/duckie/quackquack/ui/plugin/LocalQuackPlugins.kt
@@ -10,6 +10,11 @@ package team.duckie.quackquack.ui.plugin
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
 
+/**
+ * 꽥꽥 플러그인 저장소를 제공합니다.
+ * 만약 등록된 플러그인이 없다면 [EmptyQuackPlugins]를 반환합니다.
+ * 플러그인을 등록하려면 [rememberQuackPlugins]를 사용하세요.
+ */
 public val LocalQuackPlugins: ProvidableCompositionLocal<QuackPlugins> = staticCompositionLocalOf {
   EmptyQuackPlugins
 }
