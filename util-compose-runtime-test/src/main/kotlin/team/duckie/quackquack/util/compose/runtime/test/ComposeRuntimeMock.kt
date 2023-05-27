4e2ac6c97d9ee70089f210b80045119197b9981f
jisungbin <ji@sungb.in>
2023-05-27 20:50:05 +0900

    util-compose-runtime-test documentation

--- util-compose-runtime-test/src/main/kotlin/team/duckie/quackquack/util/compose/runtime/test/ComposeRuntimeMock.kt
+++ util-compose-runtime-test/src/main/kotlin/team/duckie/quackquack/util/compose/runtime/test/ComposeRuntimeMock.kt
@@ -9,6 +9,7 @@
 
 package team.duckie.quackquack.util.compose.runtime.test
 
+import android.view.Choreographer
 import androidx.compose.runtime.Applier
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Composition
@@ -22,6 +23,13 @@ import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.withContext
 
+/**
+ * [Recomposer]를 임의로 실행하여 Compose runtime 환경을 시뮬레이션합니다.
+ *
+ * @param coroutineContext [Recomposer]를 실행할 [CoroutineContext]
+ * @param withRecomposer [Recomposer]를 기반으로 실행할 람다
+ * @param withinCompositionContent [Composition.setContent] 안에서 실행할 람다
+ */
 @Suppress("TYPE_MISMATCH")
 public suspend fun composed(
   coroutineContext: CoroutineContext = TestMonotonicFrameClock(),
@@ -41,6 +49,10 @@ public suspend fun composed(
   }
 }
 
+/**
+ * 아무 동작도 하지 않는 [Applier]를 구현합니다.
+ * 만약 트리 순회가 요청되면 [IllegalStateException]이 발생합니다.
+ */
 @Suppress("EmptyFunctionBlock")
 public object EmptyApplier : Applier<Unit> {
   override val current: Unit = Unit
@@ -65,6 +77,7 @@ public object EmptyApplier : Applier<Unit> {
   }
 }
 
+/** [Choreographer]와 Latch 개념이 없는 순수한 [MonotonicFrameClock]을 구현합니다. */
 public class TestMonotonicFrameClock : MonotonicFrameClock {
   private val frameChannel = Channel<Long>()
 
