2f103735ac56cd0a9721ba5e66944c2e8a5803c2
jisungbin <ji@sungb.in>
2023-05-27 18:27:29 +0900

    ui-plugin-image-gif documentation

--- ui-plugin/image/gif/src/main/kotlin/team/duckie/quackquack/ui/plugin/image/gif/QuackImageGifPlugin.kt
+++ ui-plugin/image/gif/src/main/kotlin/team/duckie/quackquack/ui/plugin/image/gif/QuackImageGifPlugin.kt
@@ -13,6 +13,10 @@ import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import team.duckie.quackquack.ui.plugin.image.QuackImagePlugin
 
+/**
+ * `QuackImage`에 GIF 지원을 추가하는 플러그인.
+ * 내부적으로 [QuackImagePlugin.CoilImageLoader]를 사용합니다.
+ */
 @Stable
 public val QuackImageGifPlugin: QuackImagePlugin.CoilImageLoader = QuackImagePlugin.CoilImageLoader { _, _, _, _ ->
   components {
