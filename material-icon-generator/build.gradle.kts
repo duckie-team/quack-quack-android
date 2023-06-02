d4b875a224dde9dc97142b7c8b43a56a749da243
jisungbin <ji@sungb.in>
2023-06-03 01:42:07 +0900

    Add a module to convert SVGs into ImageVectors

---
+++ material-icon-generator/build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
+plugins {
+  quackquack("jvm-kotlin")
+}
+
+repositories {
+  maven("https://jitpack.io")
+}
+
+dependencies {
+  implementations(
+    libs.compose.svg.converter,
+    libs.kotlin.kotlinpoet.core,
+    // required dependencies by compose-svg-converter
+    "com.google.guava:guava:23.0",
+    "com.android.tools:sdk-common:27.2.0-alpha16",
+    "com.android.tools:common:27.2.0-alpha16",
+    "org.ogce:xpp3:1.1.6",
+  )
+}
