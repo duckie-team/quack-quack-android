22a81bb100977a5403b7a3a99012e54f6f2cce64
jisungbin <ji@sungb.in>
2023-06-04 04:24:09 +0900

    Add MultilinesSnapshotQualifier qualifier for large-width snapshot

---
+++ ui/src/test/kotlin/team/duckie/quackquack/ui/snapshot/util/qualifier.kt
@@ -0,0 +1,11 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
+package team.duckie.quackquack.ui.snapshot.util
+
+// Copied form Pixel7 qualifier
+const val MultilinesSnapshotQualifier = "w400dp-h8000dp-normal-long-notround-any-420dpi-keyshidden-nonav"
