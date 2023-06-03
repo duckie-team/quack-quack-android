0d1e3a3358aa3020db740b10a03163f50de90122
jisungbin <ji@sungb.in>
2023-06-04 04:24:20 +0900

    Improve labeling snapshot

--- ui/src/test/kotlin/team/duckie/quackquack/ui/snapshot/util/TestLayout.kt
+++ ui/src/test/kotlin/team/duckie/quackquack/ui/snapshot/util/TestLayout.kt
@@ -9,30 +9,49 @@ package team.duckie.quackquack.ui.snapshot.util
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import team.duckie.quackquack.material.theme.QuackTheme
 
 @Composable
-fun TestColumn(content: @Composable () -> Unit) {
+fun TestColumn(contentGap: Dp = 15.dp, content: @Composable () -> Unit) {
   Column(
     modifier = Modifier.wrapContentSize(),
-    verticalArrangement = Arrangement.spacedBy(15.dp),
+    verticalArrangement = Arrangement.spacedBy(contentGap),
   ) {
     QuackTheme(content)
   }
 }
 
 @Composable
-fun WithLabel(label: String, content: @Composable () -> Unit) {
+fun WithLabel(
+  label: String,
+  isTitle: Boolean = false,
+  labelGap: Dp = 4.dp,
+  contentGap: Dp = if (isTitle) 15.dp else 4.dp,
+  content: @Composable ColumnScope.() -> Unit,
+) {
   Column(
     modifier = Modifier.wrapContentSize(),
-    verticalArrangement = Arrangement.spacedBy(4.dp),
+    verticalArrangement = Arrangement.spacedBy(labelGap),
   ) {
-    BasicText(label)
-    content()
+    BasicText(
+      label,
+      style = TextStyle.Default.let { style ->
+        if (isTitle) style.copy(fontWeight = FontWeight.Bold) else style
+      },
+    )
+    Column(
+      modifier = Modifier.wrapContentSize(),
+      verticalArrangement = Arrangement.spacedBy(contentGap),
+      content = content,
+    )
   }
 }
