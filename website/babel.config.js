d299d44f285e4e32a939b02afe64269d3fbd8e33
Ji Sungbin <ji@sungb.in>
2023-06-11 22:00:25 +0900

    Cleanup website (#755)
    * Rewrite relnotes
    
    * Cleanup
    
    * Update idea configuration

--- website/babel.config.js
+++ website/babel.config.js
@@ -1,3 +1,10 @@
+/*
+ * Designed and developed by Duckie Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/quack-quack-android/blob/main/LICENSE
+ */
+
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
 };
